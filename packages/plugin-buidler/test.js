6ff93eb66d350bdf6cc9432afa2daf981e36aac5
Francisco Giordano <frangio.1@gmail.com>
2020-07-13 16:31:28 -0300

    compile contracts before running tests

--- packages/plugin-buidler/test.js
+++ packages/plugin-buidler/test.js
@@ -1,5 +1,15 @@
 const test = require('ava');
 const proc = require('promisified/child_process');
+const events = require('events');
+
+test.before(async t => {
+  const compile = proc.spawn('buidler', ['compile'], {
+    cwd: 'test',
+    stdio: 'inherit',
+  });
+  const [status] = await events.once(compile, 'exit');
+  t.is(status, 0, 'compilation failed');
+});
 
 function testFile(name) {
   test(name, async t => {
