c3a230ac584e00ebe0b2cad305bf9a0ae997f4e8
Martin Triay <martriay@gmail.com>
2020-07-17 17:26:32 -0300

    Rename eip-1967 deprecated slot to fallback

--- packages/core/src/eip-1967.ts
+++ packages/core/src/eip-1967.ts
@@ -5,35 +5,35 @@ import { EthereumProvider, getStorageAt } from './provider';
 
 export async function getAdminAddress(provider: EthereumProvider, address: string): Promise<string> {
   const ADMIN_LABEL = 'eip1967.proxy.admin';
-  const DEPRECATED_ADMIN_LABEL = 'org.zeppelinos.proxy.admin';
-  const storage = await getEip1967Storage(provider, address, ADMIN_LABEL, DEPRECATED_ADMIN_LABEL);
+  const FALLBACK_ADMIN_LABEL = 'org.zeppelinos.proxy.admin';
+  const storage = await getEip1967Storage(provider, address, ADMIN_LABEL, FALLBACK_ADMIN_LABEL);
 
   return toChecksumAddress(storage);
 }
 
 export async function getImplementationAddress(provider: EthereumProvider, address: string): Promise<string> {
   const IMPLEMENTATION_LABEL = 'eip1967.proxy.implementation';
-  const DEPRECATED_IMPLEMENTATION_LABEL = 'org.zeppelinos.proxy.implementation';
-  const storage = await getEip1967Storage(provider, address, IMPLEMENTATION_LABEL, DEPRECATED_IMPLEMENTATION_LABEL);
+  const FALLBACK_IMPLEMENTATION_LABEL = 'org.zeppelinos.proxy.implementation';
+  const storage = await getEip1967Storage(provider, address, IMPLEMENTATION_LABEL, FALLBACK_IMPLEMENTATION_LABEL);
 
   return toChecksumAddress(storage);
 }
 
-async function getEip1967Storage(provider: EthereumProvider, address: string, newSlot: string, deprecatedSlot: string): Promise<string> {
-  let storage = await getStorageAt(provider, address, toEip1967Hash(newSlot));
+async function getEip1967Storage(provider: EthereumProvider, address: string, slot: string, fallbackSlot: string): Promise<string> {
+  let storage = await getStorageAt(provider, address, toEip1967Hash(slot));
 
   if (isEmptySlot(storage)) {  
-    storage = await getStorageAt(provider, address, toDeprecatedEip1967Hash(deprecatedSlot));
+    storage = await getStorageAt(provider, address, toFallbackEip1967Hash(fallbackSlot));
   }
 
   if (isEmptySlot(storage)) {  
-    throw new Error(`Could not find "${newSlot}" nor "${deprecatedSlot}" slots in proxy storage`);
+    throw new Error(`Could not find "${slot}" nor "${fallbackSlot}" slots in proxy storage`);
   }
 
   return storage;
 }
 
-export function toDeprecatedEip1967Hash(label: string): string {
+export function toFallbackEip1967Hash(label: string): string {
   return `0x${keccak256(Buffer.from(label))}`;
 }
 
