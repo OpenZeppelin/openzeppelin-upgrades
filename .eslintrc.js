4a8bee3d8a5621a1ebf271c8a57b961328146677
Francisco Giordano <frangio.1@gmail.com>
2020-07-17 13:17:52 -0300

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      files: [ '*.ts' ],
+      parser: '@typescript-eslint/parser',
+      plugins: [
+        '@typescript-eslint',
+      ],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+    },
+    {
+      files: [ 'buidler.config.js' ],
+      globals: {
+        usePlugin: 'readonly',
+      },
+    },
+    {
+      files: [ 'ava.config.js' ],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+};
