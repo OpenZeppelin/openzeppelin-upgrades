a2a023356dbc54007513530a25b9d809c4857391
Martín Triay <martriay@gmail.com>
2020-07-17 17:46:52 -0300

    Get implementation from storage (#23)
    * Add proxy.ts and provider.ts to core
    
    * Make use of provider and proxy abstractions
    
    * Abstract hashing functions to crypto.ts
    
    * Cast storage position to bn for null check. Rename crypto.ts to eip-1967.ts
    
    * Use standalone bn.js instead of ethereumjs-util's re-export
    
    * Relocate bn.js dependency between packages
    
    * Rename eip-1967 deprecated slot to fallback
    
    * Better error handling for eip-1967 ODgetImplementationAddress
    
    * Fix {ERC,EIP}1967

---
+++ packages/core/src/eip-1967.ts
@@ -0,0 +1,49 @@
+import { toChecksumAddress, keccak256 } from 'ethereumjs-util';
+import BN from 'bn.js';
+
+import { EthereumProvider, getStorageAt } from './provider';
+
+export async function getAdminAddress(provider: EthereumProvider, address: string): Promise<string> {
+  const ADMIN_LABEL = 'eip1967.proxy.admin';
+  const FALLBACK_ADMIN_LABEL = 'org.zeppelinos.proxy.admin';
+  const storage = await getEip1967Storage(provider, address, ADMIN_LABEL, FALLBACK_ADMIN_LABEL);
+
+  return toChecksumAddress(storage);
+}
+
+export async function getImplementationAddress(provider: EthereumProvider, address: string): Promise<string> {
+  const IMPLEMENTATION_LABEL = 'eip1967.proxy.implementation';
+  const FALLBACK_IMPLEMENTATION_LABEL = 'org.zeppelinos.proxy.implementation';
+  const storage = await getEip1967Storage(provider, address, IMPLEMENTATION_LABEL, FALLBACK_IMPLEMENTATION_LABEL);
+
+  if (isEmptySlot(storage)) {
+    throw new Error(`Contract at ${address} doesn't look like an EIP 1967 proxy`);
+  }
+
+  return toChecksumAddress(storage);
+}
+
+async function getEip1967Storage(provider: EthereumProvider, address: string, slot: string, fallbackSlot: string): Promise<string> {
+  let storage = await getStorageAt(provider, address, toEip1967Hash(slot));
+
+  if (isEmptySlot(storage)) {  
+    storage = await getStorageAt(provider, address, toFallbackEip1967Hash(fallbackSlot));
+  }
+
+  return storage;
+}
+
+export function toFallbackEip1967Hash(label: string): string {
+  return `0x${keccak256(Buffer.from(label))}`;
+}
+
+export function toEip1967Hash(label: string): string {
+  const hash = keccak256(Buffer.from(label));
+  const bigNumber = new BN(hash, 'hex').sub(new BN(1));
+  return `0x${bigNumber.toString(16)}`;
+}
+
+function isEmptySlot(storage: string): boolean {
+  storage = storage.replace(/^0x/, '');
+  return new BN(storage, 'hex').isZero();
+}
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,4 +3,6 @@ export * from './impl-store';
 export * from './version';
 export * from './manifest';
 export * from './storage';
+export * from './eip-1967';
+export * from './provider';
 export * from './src-decoder';
