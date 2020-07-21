ef897ede00796818231c5aebe38f70d37f08d639
Francisco Giordano <frangio.1@gmail.com>
2020-07-21 18:12:07 -0300

    clean up test directory before running tests

--- packages/plugin-buidler/test.js
+++ packages/plugin-buidler/test.js
@@ -1,8 +1,12 @@
 const test = require('ava');
 const proc = require('promisified/child_process');
 const events = require('events');
+const { promisify } = require('util');
+
+const rimraf = promisify(require('rimraf'));
 
 test.before(async t => {
+  await rimraf('test/.openzeppelin');
   const compile = proc.spawn('buidler', ['compile'], {
     cwd: 'test',
     stdio: 'inherit',
