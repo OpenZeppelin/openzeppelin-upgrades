318fbb39e28a5f26ae16f041da0f3ba46c1edea3
Francisco Giordano <frangio.1@gmail.com>
2020-07-24 19:57:27 -0300

    remove ganache in favor of stub provider

--- packages/core/src/deployment.test.ts
+++ packages/core/src/deployment.test.ts
@@ -1,49 +1,19 @@
-import _test, { TestInterface } from 'ava';
-import * as ethers from 'ethers';
-import bre from '@nomiclabs/buidler';
-import { readArtifact } from '@nomiclabs/buidler/plugins';
-import ganache from 'ganache-core';
+import test from 'ava';
 
-import { Deployment, resumeOrDeploy } from './deployment';
-
-interface Context {
-  provider: ethers.providers.Web3Provider;
-  deploy: Record<string, () => Promise<Deployment & { contract: ethers.Contract }>>;
-}
-
-const test = _test as TestInterface<Context>;
-
-test.before(async t => {
-  const provider = new ethers.providers.Web3Provider(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ganache.provider({ blockTime: 1 }) as any,
-  );
-  const makeDeploy = async (contractName: string) => {
-    const artifact = await readArtifact(bre.config.paths.artifacts, contractName);
-    const factory = new ethers.ContractFactory(artifact.abi, artifact.bytecode, provider.getSigner());
-    return async () => {
-      const contract = await factory.deploy();
-      const { address, deployTransaction } = contract;
-      const txHash = deployTransaction.hash;
-      return { address, txHash, contract };
-    };
-  };
-
-  t.context.provider = provider;
-  t.context.deploy = {
-    DeploymentStub: await makeDeploy('DeploymentStub'),
-  };
-});
+import { resumeOrDeploy } from './deployment';
+import { stubProvider } from './stub-provider';
 
 test('deploys new contract', async t => {
-  const deployment = await resumeOrDeploy(t.context.provider, undefined, t.context.deploy.DeploymentStub);
+  const provider = stubProvider();
+  const deployment = await resumeOrDeploy(provider, undefined, provider.deploy);
   t.truthy(deployment);
-  const answer: ethers.BigNumber = await deployment.contract.answer();
-  t.is(answer.toString(), '42');
+  t.is(provider.deployCount, 1);
 });
 
 test('resumes existing deployment', async t => {
-  const first = await resumeOrDeploy(t.context.provider, undefined, t.context.deploy.DeploymentStub);
-  const second = await resumeOrDeploy(t.context.provider, first, t.context.deploy.DeploymentStub);
+  const provider = stubProvider();
+  const first = await resumeOrDeploy(provider, undefined, provider.deploy);
+  const second = await resumeOrDeploy(provider, first, provider.deploy);
   t.is(first, second);
+  t.is(provider.deployCount, 1);
 });
