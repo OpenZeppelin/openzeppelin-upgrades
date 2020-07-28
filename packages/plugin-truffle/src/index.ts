f2344ce96ea2b3170aa24e0fc96108f221d6a3dc
Francisco Giordano <frangio.1@gmail.com>
2020-07-28 18:49:10 -0300

    add json rpc id in wrapped web3 provider

--- packages/plugin-truffle/src/index.ts
+++ packages/plugin-truffle/src/index.ts
@@ -1,4 +1,5 @@
 import { promisify } from 'util';
+import crypto from 'crypto';
 
 import {
   assertUpgradeSafe,
@@ -120,8 +121,9 @@ function getProxyAdminFactory(Contract: ContractClass) {
 function wrapProvider(provider: TruffleProvider): EthereumProvider {
   const web3Send = promisify(provider.send.bind(provider));
   return {
-    async send(method: string, params?: unknown[]) {
-      const { result, error } = await web3Send({ method, params });
+    async send(method: string, params: unknown[]) {
+      const id = crypto.randomBytes(4).toString('hex');
+      const { result, error } = await web3Send({ method, params, id });
       if (error) {
         throw new Error(error.message);
       } else {
