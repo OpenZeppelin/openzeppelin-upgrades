e141caa1fae88f94cc83fcf90955afd40a2b340e
Martin Triay <martriay@gmail.com>
2020-08-06 19:08:23 -0300

    Simplify buidler admin module

--- packages/plugin-buidler/src/admin.ts
+++ packages/plugin-buidler/src/admin.ts
@@ -1,4 +1,4 @@
-import type { BuidlerRuntimeEnvironment, EthereumProvider } from '@nomiclabs/buidler/types';
+import type { BuidlerRuntimeEnvironment } from '@nomiclabs/buidler/types';
 import { Manifest, getAdminAddress } from '@openzeppelin/upgrades-core';
 import { Contract } from 'ethers';
 import { getProxyAdminFactory } from './proxy-factory';
@@ -8,41 +8,30 @@ type TransferOwnershipFunction = (proxyAdminAddress: string, newAdmin: string) =
 
 export function makeChangeProxyAdmin(bre: BuidlerRuntimeEnvironment): ChangeAdminFunction {
   return async function changeProxyAdmin(proxyAddress, newAdmin) {
-    const { provider } = bre.network;
+    const admin = await getManifestAdmin(bre, await getAdminAddress(bre.network.provider, proxyAddress));
 
-    const manifestAdminAddress = await getManifestAdminAddress(provider);
-    const admin = await getAdmin(bre, await getAdminAddress(provider, proxyAddress));
-
-    if (admin.address !== manifestAdminAddress) {
-      throw new Error('Proxy admin is not the registered ProxyAdmin contract');
-    } else if (admin.address === newAdmin) {
-      throw new Error('Cannot change Proxy adminship to the same admin');
+    if (admin.address !== newAdmin) {
+      await admin.changeProxyAdmin(proxyAddress, newAdmin);
     }
-
-    await admin.changeProxyAdmin(proxyAddress, newAdmin);
   };
 }
 
 export function makeTransferOwnership(bre: BuidlerRuntimeEnvironment): TransferOwnershipFunction {
   return async function transferOwnership(proxyAdminAddress, newOwner) {
-    const manifestAdminAddress = await getManifestAdminAddress(bre.network.provider);
-    const admin = await getAdmin(bre, proxyAdminAddress);
-
-    if (admin.address !== manifestAdminAddress) {
-      throw new Error('Proxy admin is not the registered ProxyAdmin contract');
-    }
-
+    const admin = await getManifestAdmin(bre, proxyAdminAddress);
     await admin.transferOwnerwhip(newOwner);
   };
 }
 
-async function getManifestAdminAddress(provider: EthereumProvider): Promise<string | undefined> {
-  const manifest = await Manifest.forNetwork(provider);
+async function getManifestAdmin(bre: BuidlerRuntimeEnvironment, proxyAdminAddress: string): Promise<Contract> {
+  const manifest = await Manifest.forNetwork(bre.network.provider);
   const manifestAdmin = await manifest.getAdmin();
-  return manifestAdmin?.address;
-}
-
-async function getAdmin(bre: BuidlerRuntimeEnvironment, proxyAdminAddress: string): Promise<Contract> {
   const AdminFactory = await getProxyAdminFactory(bre);
-  return AdminFactory.attach(proxyAdminAddress);
+  const admin = AdminFactory.attach(proxyAdminAddress);
+
+  if (admin.address !== manifestAdmin?.address) {
+    throw new Error('Proxy admin is not the registered ProxyAdmin contract');
+  }
+
+  return admin;
 }
