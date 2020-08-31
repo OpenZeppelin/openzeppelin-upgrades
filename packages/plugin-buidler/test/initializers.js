6a2a814ed320db81ee8dded53565106dbe238f2f
Francisco Giordano <frangio.1@gmail.com>
2020-08-31 19:27:31 -0300

    remove buidler comment

--- packages/plugin-buidler/test/initializers.js
+++ packages/plugin-buidler/test/initializers.js
@@ -10,8 +10,6 @@ async function main() {
   await upgrades.deployProxy(InitializerMissing);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch(error => {
