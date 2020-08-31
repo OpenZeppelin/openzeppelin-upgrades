af2be0ad53e8ed6a712725e566de58eeda1940af
Francisco Giordano <frangio.1@gmail.com>
2020-08-31 20:18:43 -0300

    Assert Buidler deployProxy throws error when expected

--- packages/plugin-buidler/test/initializers.js
+++ packages/plugin-buidler/test/initializers.js
@@ -3,11 +3,26 @@ const { ethers, upgrades } = require('@nomiclabs/buidler');
 
 async function main() {
   const InitializerOverloaded = await ethers.getContractFactory('InitializerOverloaded');
+
+  try {
+    await upgrades.deployProxy(InitializerOverloaded, [42]);
+    assert.fail('did not fail with overloaded initialize function');
+  } catch (e) {
+    assert(e.message.includes('multiple matching functions'));
+  }
+
   const instance = await upgrades.deployProxy(InitializerOverloaded, [42], { initializer: 'initialize(uint256)' });
   assert.strictEqual('42', (await instance.x()).toString());
 
   const InitializerMissing = await ethers.getContractFactory('InitializerMissing');
   await upgrades.deployProxy(InitializerMissing);
+
+  try {
+    await upgrades.deployProxy(InitializerMissing, [42]);
+    assert.fail('did not fail with missing initialize function');
+  } catch (e) {
+    assert(e.message.includes('no matching function'));
+  }
 }
 
 main()
