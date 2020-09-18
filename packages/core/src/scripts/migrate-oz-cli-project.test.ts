6f7759f627e658277a6b9ea7a9e7a78b20919f11
Martin Triay <martriay@gmail.com>
2020-09-18 17:32:46 -0300

    Move to snapshot testing

--- packages/core/src/scripts/migrate-oz-cli-project.test.ts
+++ packages/core/src/scripts/migrate-oz-cli-project.test.ts
@@ -5,26 +5,18 @@ import { promises as fs } from 'fs';
 
 const BASE_PATH = 'src/scripts';
 const MIGRATABLE_MANIFEST = path.join(BASE_PATH, 'migratable-manifest.test.json');
-const MIGRATED_MANIFEST = path.join(BASE_PATH, 'migrated-manifest.test.json');
-const EXPORT_FILE = path.join(BASE_PATH, 'openzeppelin-cli-export.test.json');
 
 // TODO: end to end tests including file management
 // const PROJCT_FILE = path.join(BASE_PATH, 'project-file.test.json');
 
 test('transforms manifest data', async t => {
-  const migratableData = await readJSONFile(MIGRATABLE_MANIFEST);
-  const migratedData = await readJSONFile(MIGRATED_MANIFEST);
+  const migratableData = JSON.parse(await fs.readFile(MIGRATABLE_MANIFEST, 'utf8'));
   const migrationOutput = migrateManifestsData({ rinkeby: migratableData });
-  t.deepEqual(migrationOutput.newManifestsData, { rinkeby: migratedData });
+  t.snapshot(migrationOutput.newManifestsData);
 });
 
 test('produces network export data', async t => {
-  const migratableData = await readJSONFile(MIGRATABLE_MANIFEST);
-  const exportData = await readJSONFile(EXPORT_FILE);
+  const migratableData = JSON.parse(await fs.readFile(MIGRATABLE_MANIFEST, 'utf8'));
   const migrationOutput = migrateManifestsData({ rinkeby: migratableData });
-  t.deepEqual(migrationOutput.networksExportData, exportData.networks);
+  t.snapshot(migrationOutput.networksExportData);
 });
-
-async function readJSONFile(filename: string) {
-  return JSON.parse(await fs.readFile(filename, 'utf8'));
-}
