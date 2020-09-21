ffb9910dd35135d670ee3030ea09b4ac1b17e200
Stephane <thegostep@gmail.com>
2020-09-21 09:19:09 -0400

    Update type-extensions.d.ts

--- packages/plugin-buidler/src/type-extensions.d.ts
+++ packages/plugin-buidler/src/type-extensions.d.ts
@@ -1,13 +1,14 @@
 import '@nomiclabs/buidler/types';
 
 import type { DeployFunction } from './deploy-proxy';
-import type { UpgradeFunction } from './upgrade-proxy';
+import type { UpgradeFunction, PrepareUpgradeFunction } from './upgrade-proxy';
 
 declare module '@nomiclabs/buidler/types' {
   export interface BuidlerRuntimeEnvironment {
     upgrades: {
       deployProxy: DeployFunction;
       upgradeProxy: UpgradeFunction;
+      prepareUpgrade: PrepareUpgradeFunction;
     };
   }
 }
