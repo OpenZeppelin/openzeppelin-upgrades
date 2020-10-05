b4ccff4721d882a591397fd19cc269521417cd67
Julian R <56316686+julianmrodri@users.noreply.github.com>
2020-10-04 21:32:37 -0300

    sets library functions as external

--- packages/plugin-buidler/test/contracts/ExternalLibraries.sol
+++ packages/plugin-buidler/test/contracts/ExternalLibraries.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.5.1;
 import "@nomiclabs/buidler/console.sol";
 
 library SafeMath {
-    function add(uint256 a, uint256 b) public pure returns (uint256) {
+    function add(uint256 a, uint256 b) external pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
 
         return c;
     }
 
-    function sub(uint256 a, uint256 b) public pure returns (uint256) {
+    function sub(uint256 a, uint256 b) external pure returns (uint256) {
       require(b <= a, "SafeMath: subtraction overflow");
       uint256 c = a - b;
 
@@ -24,14 +24,14 @@ library SafeMath {
 }
 
 library SafeMathV2 {
-    function add(uint256 a, uint256 b) public pure returns (uint256) {
+    function add(uint256 a, uint256 b) external pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
 
         return c;
     }
 
-    function sub(uint256 a, uint256 b) public pure returns (uint256) {
+    function sub(uint256 a, uint256 b) external pure returns (uint256) {
      require(b <= a, "SafeMath: subtraction overflow");
       uint256 c = a - b;
 
@@ -44,7 +44,7 @@ library SafeMathV2 {
 }
 
 library SafePercent {
-    function getPercent(uint256 a, uint256 pct) public pure returns (uint256) {
+    function getPercent(uint256 a, uint256 pct) external pure returns (uint256) {
         uint256 c = div(mul(a,pct),100);
         
         return c;
