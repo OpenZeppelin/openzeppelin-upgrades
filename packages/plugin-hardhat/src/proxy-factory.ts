82cc280002be40153a80914f2d667a89b77ef1a1
Martin Triay <martriay@gmail.com>
2020-11-09 18:57:54 -0300

    Fix artifact import by using new path

--- packages/plugin-hardhat/src/proxy-factory.ts
+++ packages/plugin-hardhat/src/proxy-factory.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { Signer, ContractFactory } from 'ethers';
-import AdminUpgradeabilityProxy from '@openzeppelin/upgrades-core/artifacts/AdminUpgradeabilityProxy.json';
-import ProxyAdmin from '@openzeppelin/upgrades-core/artifacts/ProxyAdmin.json';
+import AdminUpgradeabilityProxy from '@openzeppelin/upgrades-core/artifacts/contracts/proxy/AdminUpgradeabilityProxy.sol/AdminUpgradeabilityProxy.json';
+import ProxyAdmin from '@openzeppelin/upgrades-core/artifacts/contracts/proxy/ProxyAdmin.sol/ProxyAdmin.json';
 
 export async function getProxyFactory(hre: HardhatRuntimeEnvironment, signer?: Signer): Promise<ContractFactory> {
   return hre.ethers.getContractFactory(AdminUpgradeabilityProxy.abi, AdminUpgradeabilityProxy.bytecode, signer);
