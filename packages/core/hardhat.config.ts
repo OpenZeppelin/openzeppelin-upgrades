900cb6e18cca8a49d914eab35ef266e882a87b02
Martin Triay <martriay@gmail.com>
2020-11-09 19:17:09 -0300

    Fix export

--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   solidity: {
     version: '0.6.8',
   },
--- packages/plugin-hardhat/hardhat.config.ts
+++ packages/plugin-hardhat/hardhat.config.ts
@@ -4,7 +4,7 @@ import '@openzeppelin/hardhat-upgrades';
 // You have to export an object to set up your config
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
-module.exports = {
+export default {
   // This is a sample solc configuration that specifies which version of solc to use
   solidity: {
     version: '0.5.15',
