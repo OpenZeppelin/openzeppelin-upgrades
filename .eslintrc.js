1896309dc22b453f5e6cb198f6bda30b9f91465d
Francisco Giordano <frangio.1@gmail.com>
2020-11-17 15:29:24 -0300

    disable check for null assertions in linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,9 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
       extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
     },
     {
       files: ['ava.config.js'],
