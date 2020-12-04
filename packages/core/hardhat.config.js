36e50aa62782d6f1bd8ec0aa17b607e8c8d9d9a3
Francisco Giordano <frangio.1@gmail.com>
2020-12-03 21:03:09 -0300

    Set up hardhat-etherscan

--- packages/core/hardhat.config.js
+++ packages/core/hardhat.config.js
@@ -1,5 +1,17 @@
+require('dotenv/config');
+
+require('@nomiclabs/hardhat-etherscan');
+
 module.exports = {
+  networks: {
+    mainnet: {
+      url: 'https://cloudflare-eth.com',
+    },
+  },
   solidity: {
     version: '0.6.8',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
