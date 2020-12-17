4f2968fb452010f74e66934b6c36465f4e86e79e
Francisco Giordano <frangio.1@gmail.com>
2020-12-16 21:03:34 -0300

    add comment explaining ASTDereferencer

--- packages/core/src/ast-dereferencer.ts
+++ packages/core/src/ast-dereferencer.ts
@@ -4,6 +4,10 @@ import type { Node, NodeType, NodeTypeMap } from 'solidity-ast/node';
 import { curry2 } from './utils/curry';
 import type { SolcOutput } from './solc-api';
 
+// An ASTDereferencer is a function that looks up an AST node given its id, in all of the source files involved in a
+// solc run. It will generally be used together with the AST property `referencedDeclaration` (found in Identifier,
+// UserDefinedTypeName, etc.) to look up a variable definition or type definition.
+
 export interface ASTDereferencer {
   <T extends NodeType>(nodeTypes: T[]): (id: number) => NodeTypeMap[T];
   <T extends NodeType>(nodeTypes: T[], id: number): NodeTypeMap[T];
