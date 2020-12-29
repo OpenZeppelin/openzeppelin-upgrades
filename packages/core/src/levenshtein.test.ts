f94483416ef53a0b9b45ba9da0d7930243d21ccd
Francisco Giordano <frangio.1@gmail.com>
2020-12-29 20:12:20 -0300

    use t.like for levenshtein tests

--- packages/core/src/levenshtein.test.ts
+++ packages/core/src/levenshtein.test.ts
@@ -15,46 +15,50 @@ test('appended', t => {
   const a = [...'abc'];
   const b = [...'abcd'];
   const ops = levenshtein(a, b, match);
-  t.deepEqual(ops, [
-    {
+  t.like(ops, {
+    length: 1,
+    0: {
       kind: 'appended',
       updated: 'd',
     },
-  ]);
+  });
 });
 
 test('delete from end', t => {
   const a = [...'abcd'];
   const b = [...'abc'];
   const ops = levenshtein(a, b, match);
-  t.deepEqual(ops, [
-    {
+  t.like(ops, {
+    length: 1,
+    0: {
       kind: 'deleted',
       original: 'd',
     },
-  ]);
+  });
 });
 
 test('delete from middle', t => {
   const a = [...'abc'];
   const b = [...'ac'];
   const ops = levenshtein(a, b, match);
-  t.deepEqual(ops, [
-    {
+  t.like(ops, {
+    length: 1,
+    0: {
       kind: 'deleted',
       original: 'b',
     },
-  ]);
+  });
 });
 
 test('inserted', t => {
   const a = [...'abc'];
   const b = [...'azbc'];
   const ops = levenshtein(a, b, match);
-  t.deepEqual(ops, [
-    {
+  t.like(ops, {
+    length: 1,
+    0: {
       kind: 'inserted',
       updated: 'z',
     },
-  ]);
+  });
 });
