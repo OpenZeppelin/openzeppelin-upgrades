fbce442fee4bcba84724d3c4c50162b4c871e455
Francisco Giordano <frangio.1@gmail.com>
2021-01-18 20:26:32 -0300

    move bundle-pack script and fix bug with symlinks

--- scripts/bundle-pack.sh
+++ scripts/bundle-pack.sh
@@ -10,7 +10,9 @@ mv packages/core/node_modules core-node_modules
 
 for t in hardhat truffle; do
   mkdir -p "packages/plugin-$t/node_modules/@openzeppelin"
-  ln -srf packages/core "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core"
+  if [ ! -e "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core" ]; then
+    ln -sr packages/core "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core"
+  fi
   cd "packages/plugin-$t"
   sed -i.bak -e 's/^}$/,"bundledDependencies":["@openzeppelin\/upgrades-core"]}/' package.json
   npm pack --ignore-scripts
