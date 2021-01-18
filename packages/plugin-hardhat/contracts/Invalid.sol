964488b6a04829c475dfcc2473a15ffdbdbb06aa
Francisco Giordano <frangio.1@gmail.com>
2021-01-18 20:47:17 -0300

    Remove console.log from test contracts (#285)

--- packages/plugin-hardhat/contracts/Invalid.sol
+++ packages/plugin-hardhat/contracts/Invalid.sol
@@ -1,11 +1,8 @@
 pragma solidity ^0.5.1;
 
-import "hardhat/console.sol";
-
 contract Invalid {
 
     function initialize() public view {
-        console.log("This is an invalid contract, it should not be deployed");
     }
 
     function oops() public {
