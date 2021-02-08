f34f7192b2c85a705d2c65fbbbbe2378f5de9812
Yu (Alex) Zhao <cccssw@gmail.com>
2021-02-09 03:11:35 +0800

    Update wrap-provider.ts
    fixed wrapProvider when providers like ledger-provider who used Web3ProviderEngine, which throws an error with 'Web3ProviderEngine.prototype.send'

--- packages/plugin-truffle/src/wrap-provider.ts
+++ packages/plugin-truffle/src/wrap-provider.ts
@@ -6,7 +6,10 @@ import { EthereumProvider } from '@openzeppelin/upgrades-core';
 import { TruffleProvider } from './truffle';
 
 export function wrapProvider(provider: TruffleProvider): EthereumProvider {
-  const web3Send = promisify(provider.send.bind(provider));
+  let web3Send = promisify(provider.send.bind(provider));
+  if (provider['sendAsync']!=undefined){
+      web3Send = promisify(provider['sendAsync'].bind(provider));
+  }
   return {
     async send(method: string, params: unknown[]) {
       const id = crypto.randomBytes(4).toString('hex');
