02fccd184368becf0a6375ffedfd6931c40cbb48
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-04-09 11:22:36 +0200

    fix core - thanks manual testing

--- packages/core/scripts/copy-artifacts.sh
+++ packages/core/scripts/copy-artifacts.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 # copies proxy artifacts to the location they were until 1.3.0 for backwards compatibility with truffle plugin
 
-cp artifacts/contracts/proxy/{AdminUpgradeabilityProxy.sol/AdminUpgradeabilityProxy.json,ProxyAdmin.sol/ProxyAdmin.json} artifacts
+cp artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.json artifacts
+cp artifacts/@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol/ProxyAdmin.json artifacts
+cp artifacts/@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol/TransparentUpgradeableProxy.json artifacts
