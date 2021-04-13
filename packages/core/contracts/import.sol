24a725ee789ed22943e20c8a2a54a85aa5519c57
Francisco Giordano <frangio.1@gmail.com>
2021-04-12 23:57:23 -0300

    add explanation of AdminUpgradeabilityProxy

--- packages/core/contracts/import.sol
+++ packages/core/contracts/import.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
+// Kept for backwards compatibility with older versions of Hardhat and Truffle plugins.
 contract AdminUpgradeabilityProxy is TransparentUpgradeableProxy {
     constructor(address logic, address admin, bytes memory data) payable TransparentUpgradeableProxy(logic, admin, data) {}
 }
