a3640339202de752a2be8c231981fc2598e8db8f
Francisco Giordano <frangio.1@gmail.com>
2021-05-25 12:39:42 -0300

    lint

--- packages/core/src/utils/is-value-type.ts
+++ packages/core/src/utils/is-value-type.ts
@@ -1,4 +1,4 @@
-import { ParsedTypeDetailed } from "../storage/layout";
+import { ParsedTypeDetailed } from '../storage/layout';
 
 export function isValueType(type: ParsedTypeDetailed): boolean {
   return type.args === undefined || ['t_contract', 't_enum'].includes(type.head);
