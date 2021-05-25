27e10d93e3488e884c88ea2b489cac04c31fc3ad
Francisco Giordano <frangio.1@gmail.com>
2021-05-25 12:34:05 -0300

    add missing import

--- packages/core/src/utils/is-value-type.ts
+++ packages/core/src/utils/is-value-type.ts
@@ -1,3 +1,5 @@
+import { ParsedTypeDetailed } from "../storage/layout";
+
 export function isValueType(type: ParsedTypeDetailed): boolean {
   return type.args === undefined || ['t_contract', 't_enum'].includes(type.head);
 }
