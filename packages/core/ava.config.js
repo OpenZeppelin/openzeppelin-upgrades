dc1b633a03d65104356f6ae5261eb4543ae6ee8e
Francisco Giordano <frangio.1@gmail.com>
2021-08-09 19:30:53 -0300

    fix config

--- packages/core/ava.config.js
+++ packages/core/ava.config.js
@@ -1,7 +1,10 @@
 export default {
   verbose: true,
   ignoredByWatcher: ['**/*.{ts,map,tsbuildinfo}', 'artifacts', 'cache'],
-  typescript: { rewritePaths: { 'src/': 'dist/' } },
+  typescript: {
+    rewritePaths: { 'src/': 'dist/' },
+    compile: false,
+  },
   environmentVariables: {
     FORCE_COLOR: '0',
   },
