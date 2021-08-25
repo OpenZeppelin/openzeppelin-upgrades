85952a94ec6f2957e7b15311eb6cedc38d6cfaf0
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-08-25 15:56:45 +0200

    add wrapWithProvider to the truffle plugin

--- packages/plugin-truffle/src/erc1967.ts
+++ packages/plugin-truffle/src/erc1967.ts
@@ -1,20 +1,22 @@
-import { getAdminAddress, getImplementationAddress, getBeaconAddress } from '@openzeppelin/upgrades-core';
+import {
+  EthereumProvider,
+  getAdminAddress,
+  getImplementationAddress,
+  getBeaconAddress,
+} from '@openzeppelin/upgrades-core';
+
 import { wrapProvider, Options, withDefaults } from './utils';
 
+function wrapWithProvider<A, R>(getter: (provider: EthereumProvider, args: A) => R): (args: A, opts: Options) => R {
+  return (args: A, opts: Options) => {
+    const { deployer } = withDefaults(opts);
+    const provider = wrapProvider(deployer.provider);
+    return getter(provider, args);
+  };
+}
+
 export const erc1967 = {
-  getAdminAddress: async function (proxyAddress: string, opts: Options = {}): Promise<string> {
-    const { deployer } = withDefaults(opts);
-    const provider = wrapProvider(deployer.provider);
-    return getAdminAddress(provider, proxyAddress);
-  },
-  getImplementationAddress: async function (proxyAddress: string, opts: Options = {}): Promise<string> {
-    const { deployer } = withDefaults(opts);
-    const provider = wrapProvider(deployer.provider);
-    return getImplementationAddress(provider, proxyAddress);
-  },
-  getBeaconAddress: async function (proxyAddress: string, opts: Options = {}): Promise<string> {
-    const { deployer } = withDefaults(opts);
-    const provider = wrapProvider(deployer.provider);
-    return getBeaconAddress(provider, proxyAddress);
-  },
+  getAdminAddress: wrapWithProvider(getAdminAddress),
+  getImplementationAddress: wrapWithProvider(getImplementationAddress),
+  getBeaconAddress: wrapWithProvider(getBeaconAddress),
 };
