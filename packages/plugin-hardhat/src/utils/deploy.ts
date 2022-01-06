4b5641529ad19cbe825b0f461ffa9d90e3e06a91
Eric Lau <ericglau@outlook.com>
2022-01-06 10:47:53 -0500

    Override contract deployment address (#487)

--- packages/plugin-hardhat/src/utils/deploy.ts
+++ packages/plugin-hardhat/src/utils/deploy.ts
@@ -1,5 +1,7 @@
 import type { Deployment } from '@openzeppelin/upgrades-core';
+import debug from './debug';
 import type { ethers, ContractFactory } from 'ethers';
+import { getContractAddress } from 'ethers/lib/utils';
 
 export interface DeployTransaction {
   deployTransaction: ethers.providers.TransactionResponse;
@@ -9,7 +11,19 @@ export async function deploy(
   factory: ContractFactory,
   ...args: unknown[]
 ): Promise<Required<Deployment & DeployTransaction>> {
-  const { address, deployTransaction } = await factory.deploy(...args);
+  const contractInstance = await factory.deploy(...args);
+  const { deployTransaction } = contractInstance;
+
+  const address: string = getContractAddress({
+    from: await factory.signer.getAddress(),
+    nonce: deployTransaction.nonce,
+  });
+  if (address !== contractInstance.address) {
+    debug(
+      `overriding contract address from ${contractInstance.address} to ${address} for nonce ${deployTransaction.nonce}`,
+    );
+  }
+
   const txHash = deployTransaction.hash;
   return { address, txHash, deployTransaction };
 }
