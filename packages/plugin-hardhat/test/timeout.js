3bfb251f4748a1d2dac8e29cfa5ec0b0aa6d7442
Eric Lau <ericglau@outlook.com>
2022-01-28 12:27:41 -0500

    Simplify tests

--- packages/plugin-hardhat/test/timeout.js
+++ packages/plugin-hardhat/test/timeout.js
@@ -5,6 +5,8 @@ const { ethers, upgrades, network } = require('hardhat');
 test.before(async t => {
   t.context.Greeter = await ethers.getContractFactory('Greeter');
   t.context.GreeterV2 = await ethers.getContractFactory('GreeterV2');
+  t.context.GreeterProxiable = await ethers.getContractFactory('GreeterProxiable');
+  t.context.GreeterV2Proxiable = await ethers.getContractFactory('GreeterV2Proxiable');
 });
 
 test.beforeEach(async () => {
@@ -17,33 +19,41 @@ test.beforeEach(async () => {
   await network.provider.send('evm_setIntervalMining', [500]);
 });
 
-const TIMED_OUT = 'Timed out waiting for transaction';
+const TIMED_OUT = 'Timed out waiting for ';
 const USE_OPTIONS =
   'If the problem persists, adjust the polling parameters with the timeout and pollingInterval options.';
 
-test('timeout too low, long polling', async t => {
+test('timeout too low - beacon', async t => {
   const error = await t.throwsAsync(() =>
     upgrades.deployBeacon(t.context.Greeter, { timeout: 1, pollingInterval: 1000 }),
   );
   t.true(error.message.includes(TIMED_OUT) && error.message.includes(USE_OPTIONS));
 });
 
-test('timeout too low, short polling', async t => {
-  const error = await t.throwsAsync(() => upgrades.deployBeacon(t.context.Greeter, { timeout: 1, pollingInterval: 1 }));
+test('timeout too low - proxy', async t => {
+  const error = await t.throwsAsync(() =>
+    upgrades.deployProxy(t.context.GreeterProxiable, ['Hello, Hardhat!'], {
+      kind: 'uups',
+      timeout: 1,
+      pollingInterval: 0,
+    }),
+  );
   t.true(error.message.includes(TIMED_OUT) && error.message.includes(USE_OPTIONS));
 });
 
-test('timeout too low, 0 ms polling', async t => {
-  const error = await t.throwsAsync(() => upgrades.deployBeacon(t.context.Greeter, { timeout: 1, pollingInterval: 0 }));
-  t.true(error.message.includes(TIMED_OUT) && error.message.includes(USE_OPTIONS));
+test('good timeout - beacon', async t => {
+  await upgrades.deployBeacon(t.context.Greeter, {
+    timeout: 2000,
+    pollingInterval: 1000,
+  });
 });
 
-test('good timeout, long polling', async t => {
-  await upgrades.deployBeacon(t.context.Greeter, { timeout: 2000, pollingInterval: 1000 });
-});
-
-test('good timeout, short polling', async t => {
-  await upgrades.deployBeacon(t.context.Greeter, { timeout: 2000, pollingInterval: 10 });
+test('good timeout - proxy', async t => {
+  await upgrades.deployProxy(t.context.Greeter, ['Hello, Hardhat!'], {
+    kind: 'transparent',
+    timeout: 2000,
+    pollingInterval: 10,
+  });
 });
 
 test('infinite timeout, long polling', async t => {
@@ -62,8 +72,8 @@ test('single option', async t => {
   await upgrades.deployBeacon(t.context.Greeter, { pollingInterval: 0 });
 });
 
-test('upgrade', async t => {
-  // automine to immediately deploy a new proxy to use in below tests
+test('upgrade beacon', async t => {
+  // automine to immediately deploy a new beacon to use in below tests
   await network.provider.send('evm_setAutomine', [true]);
   const beacon = await upgrades.deployBeacon(t.context.Greeter, {
     timeout: 0,
@@ -79,4 +89,24 @@ test('upgrade', async t => {
 
   // upgrade: infinite timeout
   await upgrades.upgradeBeacon(beacon, t.context.GreeterV2, { timeout: 0, pollingInterval: 10 });
+});
+
+test('upgrade proxy', async t => {
+  // automine to immediately deploy a new proxy to use in below tests
+  await network.provider.send('evm_setAutomine', [true]);
+  const proxy = await upgrades.deployProxy(t.context.GreeterProxiable, ['Hello, Hardhat!'], {
+    kind: 'uups',
+    timeout: 0,
+    pollingInterval: 0,
+  });
+  await network.provider.send('evm_setAutomine', [false]);
+
+  // upgrade: timeout too low
+  const error = await t.throwsAsync(() =>
+    upgrades.upgradeProxy(proxy, t.context.GreeterV2Proxiable, { timeout: 1, pollingInterval: 1 }),
+  );
+  t.true(error.message.includes(TIMED_OUT) && error.message.includes(USE_OPTIONS));
+
+  // upgrade: infinite timeout
+  await upgrades.upgradeProxy(proxy, t.context.GreeterV2Proxiable, { timeout: 0, pollingInterval: 10 });
 });
