2fbc191ff3ea99a1cce001575210de3219f0ef1b
Francisco Giordano <frangio.1@gmail.com>
2022-02-18 14:39:38 -0300

    remove unused solc settings

--- packages/plugin-hardhat/hardhat.config.js
+++ packages/plugin-hardhat/hardhat.config.js
@@ -1,20 +1,12 @@
 require('@openzeppelin/hardhat-upgrades');
 
-const settings = {
-  outputSelection: {
-    '*': {
-      '*': ['storageLayout'],
-    },
-  },
-};
-
 module.exports = {
   solidity: {
     compilers: [
-      { version: '0.8.9', settings },
-      { version: '0.7.6', settings },
-      { version: '0.6.12', settings },
-      { version: '0.5.17', settings },
+      { version: '0.8.9' },
+      { version: '0.7.6' },
+      { version: '0.6.12' },
+      { version: '0.5.17' },
     ],
   },
 };
