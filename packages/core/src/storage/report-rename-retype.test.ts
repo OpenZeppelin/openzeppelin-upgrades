43c74638d84ce015c6d00e897c3b7843bf3be8f5
JulissaDantes <julissa@openzeppelin.com>
2022-03-02 16:36:26 -0400

    Add different test file

---
+++ packages/core/src/storage/report-rename-retype.test.ts
@@ -0,0 +1,69 @@
+import _test, { TestFn } from 'ava';
+import { ContractDefinition } from 'solidity-ast';
+import { ASTDereferencer, findAll } from 'solidity-ast/utils';
+import { artifacts } from 'hardhat';
+
+import { SolcOutput } from '../solc-api';
+import { astDereferencer } from '../ast-dereferencer';
+import { extractStorageLayout } from './extract';
+import { StorageLayoutComparator } from './compare';
+import { StorageLayout, getDetailedLayout } from './layout';
+
+interface Context {
+  extractStorageLayout: (contract: string) => ReturnType<typeof extractStorageLayout>;
+}
+
+const test = _test as TestFn<Context>;
+
+const dummyDecodeSrc = () => 'file.sol:1';
+const testContracts = [
+  'contracts/test/ValidationsNatspec.sol:RenameV1',
+  'contracts/test/ValidationsNatspec.sol:RenameV2',
+  'contracts/test/ValidationsNatspec.sol:RetypeV1',
+  'contracts/test/ValidationsNatspec.sol:RetypeV2',
+];
+
+test.before(async t => {
+  const contracts: Record<string, ContractDefinition> = {};
+  const deref: Record<string, ASTDereferencer> = {};
+  for (const contract of testContracts) {
+    const buildInfo = await artifacts.getBuildInfo(contract);
+    if (buildInfo === undefined) {
+      throw new Error(`Build info not found for contract ${contract}`);
+    }
+    const solcOutput = buildInfo.output;
+    for (const def of findAll('ContractDefinition', solcOutput.sources['contracts/test/ValidationsNatspec.sol'].ast)) {
+      contracts[def.name] = def;
+      deref[def.name] = astDereferencer(solcOutput);
+    }
+  }
+
+  t.context.extractStorageLayout = name => extractStorageLayout(contracts[name], dummyDecodeSrc, deref[name]);
+});
+
+function getReport(original: StorageLayout, updated: StorageLayout) {
+  const originalDetailed = getDetailedLayout(original);
+  const updatedDetailed = getDetailedLayout(updated);
+  const comparator = new StorageLayoutComparator();
+  return comparator.compareLayouts(originalDetailed, updatedDetailed);
+}
+
+test.only('rename new', t => {
+  console.log('im here');
+  const v1 = t.context.extractStorageLayout('RenameV1');
+  console.log('im here0');
+  const v2 = t.context.extractStorageLayout('RenameV2');
+  console.log('im here1');
+  const report = getReport(v1, v2);
+  console.log('im here2', report);
+  t.pass();
+});
+
+test.only('retype new', t => {
+  const v1 = t.context.extractStorageLayout('RetypeV1');
+  const v2 = t.context.extractStorageLayout('RetypeV2');
+  const report = getReport(v1, v2);
+
+  console.log('im rtyped here2', report);
+  t.pass();
+});
