ee62273e0e542eb6023587456ad6dcb49f0b05b5
Eric Lau <ericglau@outlook.com>
2022-05-03 14:53:07 -0400

    Fix lint

--- packages/core/src/validate/run.ts
+++ packages/core/src/validate/run.ts
@@ -248,17 +248,19 @@ function* getStateVariableErrors(
 }
 
 function getReferencedLibraryIds(contractDef: ContractDefinition): number[] {
-  const implicitUsage = [...findAll('UsingForDirective', contractDef)].map(
-    usingForDirective => {
+  const implicitUsage = [...findAll('UsingForDirective', contractDef)]
+    .map(usingForDirective => {
       if (usingForDirective.libraryName !== undefined) {
         return usingForDirective.libraryName.referencedDeclaration;
       } else if (usingForDirective.functionList !== undefined) {
         return usingForDirective.functionList.map(func => func.function.referencedDeclaration);
       } else {
-        throw new Error('Broken invariant: either UsingForDirective.libraryName or UsingForDirective.functionList should be defined');
+        throw new Error(
+          'Broken invariant: either UsingForDirective.libraryName or UsingForDirective.functionList should be defined',
+        );
       }
-    }
-  ).flat();
+    })
+    .flat();
 
   const explicitUsage = [...findAll('Identifier', contractDef)]
     .filter(identifier => identifier.typeDescriptions.typeString?.match(/^type\(library/))
