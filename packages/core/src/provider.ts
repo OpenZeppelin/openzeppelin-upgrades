a92acdb75eb76b0b0f55fe0d2c200177b305c650
Jamie Brough <jamie@arpeggi.io>
2022-05-13 15:37:49 +0000

    include openethereum dev chain in recognised dev chains

--- packages/core/src/provider.ts
+++ packages/core/src/provider.ts
@@ -109,7 +109,8 @@ export async function isDevelopmentNetwork(provider: EthereumProvider): Promise<
   const chainId = await getChainId(provider);
   //  1337 => ganache and geth --dev
   // 31337 => hardhat network
-  if (chainId === 1337 || chainId === 31337) {
+  console.debug("chain id = ", chainId)
+  if (chainId === 17 || chainId === 1337 || chainId === 31337) {
     return true;
   } else {
     const clientVersion = await getClientVersion(provider);
