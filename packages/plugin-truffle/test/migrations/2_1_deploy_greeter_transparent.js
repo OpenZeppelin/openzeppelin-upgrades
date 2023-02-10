038a7aed178bebf3375deff9b08bfb176c27077c
Eric Lau <ericglau@outlook.com>
2023-02-10 12:29:22 -0300

    Add txhash for beacon proxy, assert in migrations

--- packages/plugin-truffle/test/migrations/2_1_deploy_greeter_transparent.js
+++ packages/plugin-truffle/test/migrations/2_1_deploy_greeter_transparent.js
@@ -2,8 +2,12 @@ const Greeter = artifacts.require('Greeter');
 const GreeterV2 = artifacts.require('GreeterV2');
 
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+const assert = require('assert');
 
 module.exports = async function (deployer) {
   const g = await deployProxy(Greeter, ['Hello Truffle'], { deployer, kind: 'transparent' });
+  assert.equal(Greeter.address, g.address);
+  assert.equal(Greeter.transactionHash, g.transactionHash);
+
   await upgradeProxy(g, GreeterV2, { deployer });
 };
--- packages/plugin-truffle/test/migrations/2_2_deploy_greeter_uups.js
+++ packages/plugin-truffle/test/migrations/2_2_deploy_greeter_uups.js
@@ -2,8 +2,12 @@ const Greeter = artifacts.require('GreeterProxiable');
 const GreeterV2 = artifacts.require('GreeterV2Proxiable');
 
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+const assert = require('assert');
 
 module.exports = async function (deployer) {
   const g = await deployProxy(Greeter, ['Hello Truffle'], { deployer, kind: 'uups' });
+  assert.equal(Greeter.address, g.address);
+  assert.equal(Greeter.transactionHash, g.transactionHash);
+
   await upgradeProxy(g, GreeterV2, { deployer });
 };
