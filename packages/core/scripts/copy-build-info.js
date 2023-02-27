84809016aaab46c4309b613bb5268568f1b6af5c
Eric Lau <ericglau@outlook.com>
2023-02-27 11:43:07 -0500

    Additional assertions

--- packages/core/scripts/copy-build-info.js
+++ packages/core/scripts/copy-build-info.js
@@ -26,15 +26,40 @@ const buildInfoField = readJSON(
 ).buildInfo;
 const jsonRelativePath = `artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/${buildInfoField}`;
 
+// Assert that all deployable proxy artifacts use the same build-info file
+assert(
+  buildInfoField ===
+    readJSON('artifacts/@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol/BeaconProxy.dbg.json').buildInfo,
+);
+assert(
+  buildInfoField ===
+    readJSON('artifacts/@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol/UpgradeableBeacon.dbg.json')
+      .buildInfo,
+);
+assert(
+  buildInfoField ===
+    readJSON(
+      'artifacts/@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol/TransparentUpgradeableProxy.dbg.json',
+    ).buildInfo,
+);
+assert(
+  buildInfoField ===
+    readJSON('artifacts/@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol/ProxyAdmin.dbg.json').buildInfo,
+);
+
 const buildInfo = readJSON(jsonRelativePath);
 const reducedInfo = { solcLongVersion: buildInfo.solcLongVersion, input: buildInfo.input };
 
 const sources = reducedInfo.input.sources;
+
+// Assert that all deployable proxy artifacts exist in ERC1967's build-info file
 assert(hasProperty(sources, '@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol'));
 assert(hasProperty(sources, '@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol'));
 assert(hasProperty(sources, '@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol'));
 assert(hasProperty(sources, '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol'));
 assert(hasProperty(sources, '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol'));
+
+// Assert that the build-info file does NOT contain test contracts
 assert(!hasPropertyStartsWith(sources, 'contracts/test'));
 
 writeJSON('artifacts/build-info.json', reducedInfo);
