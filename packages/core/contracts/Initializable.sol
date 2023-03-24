f05fb1a4500f872eb5c3b21c9616bb3403d8657e
zhiqiangxu <652732310@qq.com>
2023-03-24 22:24:02 +0800

    optimize isConstructor

--- packages/core/contracts/Initializable.sol
+++ packages/core/contracts/Initializable.sol
@@ -51,14 +51,12 @@ contract Initializable {
 
   /// @dev Returns true if and only if the function is running in the constructor
   function isConstructor() private view returns (bool) {
-    // extcodesize checks the size of the code stored in an address, and
-    // address returns the current address. Since the code is still not
+    // codesize checks the size of the current code. Since the code is still not
     // deployed when running a constructor, any checks on its code size will
     // yield zero, making it an effective way to detect if a contract is
     // under construction or not.
-    address self = address(this);
     uint256 cs;
-    assembly { cs := extcodesize(self) }
+    assembly { cs := codesize() }
     return cs == 0;
   }
 
