e26a5c1de5ca676cb96f797ffb897937af4e40b2
Eric Lau <ericglau@outlook.com>
2023-03-31 16:52:41 -0400

    Add proxy deployed test

--- packages/plugin-hardhat/test/platform-deploy-proxy.js
+++ packages/plugin-hardhat/test/platform-deploy-proxy.js
@@ -8,9 +8,12 @@ const { ethers } = hre;
 const manifest = require('@openzeppelin/upgrades-core/dist/manifest');
 
 const IMPL_ID = 'abc';
+
+const PROXY_ADDR = '0x1';
+const PROXY_TX_HASH = '0x2';
 const PROXY_ID = 'def';
 
-test.before(async t => {
+test.beforeEach(async t => {
   const stub = sinon.stub();
   stub.onCall(0).returns(IMPL_ID);
   stub.onCall(1).returns(PROXY_ID);
@@ -38,7 +41,7 @@ test.afterEach.always(() => {
 test('deploy proxy', async t => {
   const { deployProxy, GreeterProxiable } = t.context;
 
-  const inst = await deployProxy(GreeterProxiable, ['Hello World'], { kind: 'uups' });
+  const inst = await deployProxy(GreeterProxiable, ['Hello World']);
   t.not(inst.address, undefined);
 
   // check that manifest has deployment ids
@@ -57,4 +60,42 @@ test('deploy proxy - unsafe', async t => {
   const { deployProxy, Invalid } = t.context;
 
   await t.throwsAsync(() => deployProxy(Invalid), undefined, 'Contract `Invalid` is not upgrade safe');
+});
+
+test('deployed calls wait for deployment', async t => {
+  const { GreeterProxiable } = t.context;
+
+  // deploy impl since we aren't testing that here
+  await hre.upgrades.deployImplementation(GreeterProxiable);
+
+  // stub proxy deployment
+  const deployStub = sinon.stub();
+  deployStub.returns({
+    address: PROXY_ADDR,
+    txHash: PROXY_TX_HASH,
+    deployTransaction: undefined,
+    deploymentId: PROXY_ID,
+  });
+
+  // stub the waitForDeployment function
+  const waitStub = sinon.stub();
+
+  const deployProxy = proxyquire('../dist/deploy-proxy', {
+    './platform/deploy': {
+      platformDeploy: deployStub,
+      '@global': true,
+    },
+    './platform/utils': {
+      waitForDeployment: waitStub,
+      setPlatformDefaults: (hre, platformModule, opts) => {
+        opts.platform = true;
+      },
+      '@global': true,
+    },
+  }).makeDeployProxy(hre, true);
+
+  const inst = await deployProxy(GreeterProxiable, ['Hello World']);
+  await inst.deployed();
+
+  t.is(waitStub.callCount, 1);
 });
