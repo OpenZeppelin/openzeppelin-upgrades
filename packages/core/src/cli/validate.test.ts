79e05e9fe5440e078a81c7f4222dfcc164e15f5d
Eric Lau <ericglau@outlook.com>
2023-06-02 16:56:49 -0400

    Add withDefaults test

--- packages/core/src/cli/validate.test.ts
+++ packages/core/src/cli/validate.test.ts
@@ -1,7 +1,8 @@
 import test from 'ava';
 import minimist from 'minimist';
-import { USAGE, DETAILS, handleHelp, main } from './validate';
+import { USAGE, DETAILS, handleHelp, main, withDefaults } from './validate';
 import sinon from 'sinon';
+import { errorKinds } from '../validate/run';
 
 test('help', t => {
   const parsedArgs = minimist(['validate', '--help']);
@@ -62,5 +63,53 @@ test('no help needed', t => {
 test('invalid options', t => {
   t.throws(() => main(['validate', 'build-info.json', '--foo', '--bar', 'xyz']), {
     message: `Invalid options: foo, bar`,
+  });
+});
+
+test('withDefaults - empty', t => {
+  const parsedArgs = minimist(['validate', 'build-info.json']);
+  const opts = withDefaults(parsedArgs);
+  t.is(opts.unsafeAllowRenames, false);
+  t.is(opts.unsafeSkipStorageCheck, false);
+  t.is(opts.unsafeAllowCustomTypes, false);
+  t.is(opts.unsafeAllowLinkedLibraries, false);
+  t.deepEqual(opts.unsafeAllow, []);
+});
+
+test('withDefaults - some', t => {
+  const parsedArgs = minimist(['validate', 'build-info.json', '--unsafeAllowRenames', '--unsafeAllow', 'selfdestruct']);
+  const opts = withDefaults(parsedArgs);
+  t.is(opts.unsafeAllowRenames, true);
+  t.is(opts.unsafeSkipStorageCheck, false);
+  t.is(opts.unsafeAllowCustomTypes, false);
+  t.is(opts.unsafeAllowLinkedLibraries, false);
+  t.deepEqual(opts.unsafeAllow, ['selfdestruct']);
+});
+
+test('withDefaults - all', t => {
+  const parsedArgs = minimist([
+    'validate',
+    'build-info.json',
+    '--unsafeAllowRenames',
+    '--unsafeSkipStorageCheck',
+    '--unsafeAllowCustomTypes',
+    '--unsafeAllowLinkedLibraries',
+    '--unsafeAllow',
+    ...errorKinds,
+  ]);
+  const opts = withDefaults(parsedArgs);
+  t.is(opts.unsafeAllowRenames, true);
+  t.is(opts.unsafeSkipStorageCheck, true);
+  t.is(opts.unsafeAllowCustomTypes, true);
+  t.is(opts.unsafeAllowLinkedLibraries, true);
+  t.true(opts.unsafeAllow.every((kind: string) => errorKinds.includes(kind as typeof errorKinds[number])));
+});
+
+test('withDefaults - invalid unsafeAllow', t => {
+  const parsedArgs = minimist(['validate', 'build-info.json', '--unsafeAllow', 'foo']);
+  t.throws(() => withDefaults(parsedArgs), {
+    message: `Invalid option: --unsafeAllow "foo". Supported values for the --unsafeAllow option are: ${errorKinds.join(
+      ', ',
+    )}`,
   });
 });
