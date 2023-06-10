b5a3c2768604ba705c292ab2c4fc07f3ab42f1b5
Eric Lau <ericglau@outlook.com>
2023-06-09 23:38:12 -0400

    Remove unnecessary test

--- packages/core/src/cli/validate/upgradeability-assessment.test.ts
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import { ReferenceContractNotFound } from './upgradeability-assessment';
-
-test('reference contract not found', async t => {
-  const e = t.throws(() => {
-    throw new ReferenceContractNotFound('REFERENCE', 'ORIGIN');
-  });
-  t.is(e?.message, 'Could not find contract REFERENCE referenced in ORIGIN.');
-});
