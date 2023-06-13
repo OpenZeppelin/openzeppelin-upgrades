04b43e627dbf48ae648566f096987a6e0cd1ff7c
Eric Lau <ericglau@outlook.com>
2023-06-12 21:13:02 -0400

    Test CLI directly for validate

--- packages/core/src/cli/cli.test.ts
+++ packages/core/src/cli/cli.test.ts
@@ -1,14 +1,75 @@
 import test from 'ava';
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import { promises as fs } from 'fs';
+import path from 'path';
+import os from 'os';
+import rimrafAsync from 'rimraf';
+import { artifacts } from 'hardhat';
+
 const execAsync = promisify(exec);
+const rimraf = promisify(rimrafAsync);
+
+const CLI = 'node dist/cli/cli.js';
 
 test('help', async t => {
-  const output = (await execAsync('node dist/cli/cli.js validate --help')).stdout;
+  const output = (await execAsync(`${CLI} validate --help`)).stdout;
   t.snapshot(output);
 });
 
 test('no args', async t => {
-  const output = (await execAsync('node dist/cli/cli.js')).stdout;
+  const output = (await execAsync(CLI)).stdout;
   t.snapshot(output);
+});
+
+test('invalid options', async t => {
+  await t.throwsAsync(execAsync(`${CLI} validate build-info.json --foo --bar xyz`), {
+    message: /Invalid options: foo, bar/,
+  });
+});
+
+test('invalid command', async t => {
+  await t.throwsAsync(execAsync(`${CLI} invalid`), {
+    message: /Invalid command: invalid. Supported commands are: validate/,
+  });
+});
+
+test('validate - errors', async t => {
+  const temp = await fs.mkdtemp(path.join(os.tmpdir(), 'upgrades-core-test-'));
+  const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/Validate.sol:Safe`);
+  await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
+
+  try {
+    const error = await t.throwsAsync(execAsync(`${CLI} validate ${temp}`));
+    const expectation: string[] = [`Stdout: ${(error as any).stdout}`, `Stderr: ${(error as any).stderr}`];
+    t.snapshot(expectation.join('\n'));
+  } finally {
+    await rimraf(temp);
+  }
+});
+
+test('validate - no upgradeable', async t => {
+  const temp = await fs.mkdtemp(path.join(os.tmpdir(), 'upgrades-core-test-'));
+  const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/Storage088.sol:Storage088`);
+  await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
+
+  try {
+    const output = (await execAsync(`${CLI} validate ${temp}`)).stdout;
+    t.snapshot(output);
+  } finally {
+    await rimraf(temp);
+  }
+});
+
+test('validate - ok', async t => {
+  const temp = await fs.mkdtemp(path.join(os.tmpdir(), 'upgrades-core-test-'));
+  const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/Annotation.sol:Annotation`);
+  await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
+
+  try {
+    const output = (await execAsync(`${CLI} validate ${temp}`)).stdout;
+    t.snapshot(output);
+  } finally {
+    await rimraf(temp);
+  }
 });
--- packages/core/src/cli/validate.test.ts
+++ packages/core/src/cli/validate.test.ts
@@ -1,31 +1,15 @@
 import test from 'ava';
 
-import { promises as fs } from 'fs';
-import rimrafAsync from 'rimraf';
-import path from 'path';
-import os from 'os';
-import util from 'util';
 import minimist from 'minimist';
-import { getFunctionArgs, main, withDefaults } from './validate';
+import { getFunctionArgs, withDefaults } from './validate';
 import sinon from 'sinon';
 import { errorKinds } from '../validate/run';
-import { artifacts } from 'hardhat';
-
-const rimraf = util.promisify(rimrafAsync);
 
 test.afterEach.always(() => {
   sinon.restore();
 });
 
-test('invalid command', t => {
-  const parsedArgs = minimist(['invalid']);
-  const extraArgs = parsedArgs._;
-  t.throws(() => getFunctionArgs(parsedArgs, extraArgs), {
-    message: `Invalid command: invalid. Supported commands are: validate`,
-  });
-});
-
-test('command only', t => {
+test('getFunctionArgs - command only', t => {
   const parsedArgs = minimist(['validate']);
   const extraArgs = parsedArgs._;
   const functionArgs = getFunctionArgs(parsedArgs, extraArgs);
@@ -41,7 +25,7 @@ test('command only', t => {
   }
 });
 
-test('command with arg', t => {
+test('getFunctionArgs - command with arg', t => {
   const parsedArgs = minimist(['validate', 'build-info.json']);
   const extraArgs = parsedArgs._;
   const functionArgs = getFunctionArgs(parsedArgs, extraArgs);
@@ -55,12 +39,6 @@ test('command with arg', t => {
     t.is(functionArgs.opts.unsafeAllowLinkedLibraries, false);
     t.deepEqual(functionArgs.opts.unsafeAllow, []);
   }
-});
-
-test('invalid options', async t => {
-  await t.throwsAsync(main(['validate', 'build-info.json', '--foo', '--bar', 'xyz']), {
-    message: `Invalid options: foo, bar`,
-  });
 });
 
 test('withDefaults - empty', t => {
@@ -116,87 +94,3 @@ test('withDefaults - invalid unsafeAllow', t => {
     )}`,
   });
 });
-
-test.serial('main - errors', async t => {
-  process.chdir(await fs.mkdtemp(path.join(os.tmpdir(), 'upgrades-core-test-')));
-  await fs.mkdir('main-errors');
-
-  const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/Validate.sol:Safe`);
-  await fs.writeFile('main-errors/1.json', JSON.stringify(buildInfo));
-
-  const { consoleLogStub, consoleErrorStub, messages } = stubConsole();
-
-  const exitCode = process.exitCode;
-  try {
-    await t.notThrowsAsync(main(['validate', 'main-errors']));
-    t.is(process.exitCode, 1);
-
-    t.true(consoleLogStub.called);
-    t.true(consoleErrorStub.calledWith('\nUpgrade safety checks completed with the following errors:'));
-    t.snapshot(messages);
-  } finally {
-    process.exitCode = exitCode;
-    await rimraf(process.cwd());
-  }
-});
-
-test.serial('main - no upgradeable', async t => {
-  process.chdir(await fs.mkdtemp(path.join(os.tmpdir(), 'upgrades-core-test-')));
-  await fs.mkdir('main-no-errors');
-
-  const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/Storage088.sol:Storage088`);
-  await fs.writeFile('main-no-errors/1.json', JSON.stringify(buildInfo));
-
-  const { consoleLogStub, consoleErrorStub, messages } = stubConsole();
-
-  const exitCode = process.exitCode;
-  try {
-    await t.notThrowsAsync(main(['validate', 'main-no-errors']));
-    t.is(process.exitCode, 0);
-
-    t.true(consoleLogStub.calledWith('\nNo upgradeable contracts detected.'));
-    t.true(consoleErrorStub.notCalled);
-    t.snapshot(messages);
-  } finally {
-    process.exitCode = exitCode;
-    await rimraf(process.cwd());
-  }
-});
-
-test.serial('main - ok', async t => {
-  process.chdir(await fs.mkdtemp(path.join(os.tmpdir(), 'upgrades-core-test-')));
-  await fs.mkdir('main-no-errors');
-
-  const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/Annotation.sol:Annotation`);
-  await fs.writeFile('main-no-errors/1.json', JSON.stringify(buildInfo));
-
-  const { consoleLogStub, consoleErrorStub, messages } = stubConsole();
-
-  const exitCode = process.exitCode;
-  try {
-    await t.notThrowsAsync(main(['validate', 'main-no-errors']));
-    t.is(process.exitCode, 0);
-
-    t.true(consoleLogStub.calledWith('\nUpgrade safety checks completed successfully.'));
-    t.true(consoleErrorStub.notCalled);
-    t.snapshot(messages);
-  } finally {
-    process.exitCode = exitCode;
-    await rimraf(process.cwd());
-  }
-});
-
-function stubConsole() {
-  const consoleLogStub = sinon.stub(console, 'log');
-  const consoleErrorStub = sinon.stub(console, 'error');
-
-  const messages: string[] = [];
-
-  consoleLogStub.callsFake((...args: string[]) => {
-    messages.push(args.join(' '));
-  });
-  consoleErrorStub.callsFake((...args: string[]) => {
-    messages.push(args.join(' '));
-  });
-  return { consoleLogStub, consoleErrorStub, messages };
-}
