7058524dc2634810f81402b7f7da2fa02871eb08
Eric Lau <ericglau@outlook.com>
2023-06-19 15:46:31 -0400

    Fix attach

--- packages/plugin-hardhat/src/utils/ethers.ts
+++ packages/plugin-hardhat/src/utils/ethers.ts
@@ -3,8 +3,8 @@ import { Contract, ContractFactory, ContractRunner, Signer } from 'ethers';
 /**
  * Attaches a ContractFactory to an address and returns a Contract instance.
  */
-export function attach(contractFactory: ContractFactory, address: string) {
-  return new Contract(address, contractFactory.interface, contractFactory.runner);
+export function attach(contractFactory: ContractFactory, address: string): Contract {
+  return contractFactory.attach(address) as Contract; // Needed because ethers attach returns a BaseContract.
 }
 
 /**
