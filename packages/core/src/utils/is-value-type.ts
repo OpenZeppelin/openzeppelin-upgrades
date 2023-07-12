57ae09465011d9bb18667d353fa9f39e98536ccc
Igor Matuszewski <Xanewok@gmail.com>
2023-07-12 22:09:05 +0200

    Support user-defined value types in mappings (#844)
    Co-authored-by: Eric Lau <ericglau@outlook.com>

--- packages/core/src/utils/is-value-type.ts
+++ packages/core/src/utils/is-value-type.ts
@@ -1,5 +1,5 @@
 import { ParsedTypeDetailed } from '../storage/layout';
 
 export function isValueType(type: ParsedTypeDetailed): boolean {
-  return type.args === undefined || ['t_contract', 't_enum'].includes(type.head);
+  return type.args === undefined || ['t_contract', 't_enum', 't_userDefinedValueType'].includes(type.head);
 }
