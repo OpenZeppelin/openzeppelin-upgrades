89890507ab2cd353568f0553354225fe797d73bb
Eric Lau <ericglau@outlook.com>
2023-09-14 10:36:39 -0400

    Improve test

--- packages/core/src/storage-namespaced-udvt.test.ts
+++ packages/core/src/storage-namespaced-udvt.test.ts
@@ -10,16 +10,14 @@ import { extractStorageLayout } from './storage/extract';
 import { solcInputOutputDecoder } from './src-decoder';
 
 interface Context {
-  extractStorageLayout: (contract: string) => ReturnType<typeof extractStorageLayout>;
+  extractStorageLayout: (contract: string, layoutInfo: boolean) => ReturnType<typeof extractStorageLayout>;
 }
 
 const test = _test as TestFn<Context>;
 
 test.before(async t => {
   const origBuildInfo = await artifacts.getBuildInfo('contracts/test/NamespacedUDVT.sol:NamespacedUDVT');
-  const namespacedBuildInfo = await artifacts.getBuildInfo(
-    'contracts/test/NamespacedUDVTLayout.sol:NamespacedUDVT',
-  );
+  const namespacedBuildInfo = await artifacts.getBuildInfo('contracts/test/NamespacedUDVTLayout.sol:NamespacedUDVT');
 
   if (origBuildInfo === undefined || namespacedBuildInfo === undefined) {
     throw new Error('Build info not found');
@@ -34,10 +32,7 @@ test.before(async t => {
   const namespacedStorageLayouts: Record<string, StorageLayout> = {};
 
   const origContractDefs = [];
-  for (const def of findAll(
-    'ContractDefinition',
-    origSolcOutput.sources['contracts/test/NamespacedUDVT.sol'].ast,
-  )) {
+  for (const def of findAll('ContractDefinition', origSolcOutput.sources['contracts/test/NamespacedUDVT.sol'].ast)) {
     origContractDefs.push(def);
   }
   const namespacedContractDefs = [];
@@ -68,24 +63,39 @@ test.before(async t => {
   const namespacedDeref = astDereferencer(namespacedSolcOutput);
 
   const decodeSrc = solcInputOutputDecoder(origBuildInfo.input, origSolcOutput);
-  t.context.extractStorageLayout = name =>
-    extractStorageLayout(origContracts[name], decodeSrc, origDeref, origStorageLayouts[name], {
-      deref: namespacedDeref,
-      contractDef: namespacedContracts[name],
-      storageLayout: namespacedStorageLayouts[name],
-    });
+  t.context.extractStorageLayout = (name, layoutInfo) =>
+    extractStorageLayout(
+      origContracts[name],
+      decodeSrc,
+      origDeref,
+      origStorageLayouts[name],
+      layoutInfo
+        ? {
+            deref: namespacedDeref,
+            contractDef: namespacedContracts[name],
+            storageLayout: namespacedStorageLayouts[name],
+          }
+        : undefined,
+    );
+});
+
+test('user defined value types - layout info', async t => {
+  const v1 = t.context.extractStorageLayout('NamespacedUDVT', true);
+  const v2 = t.context.extractStorageLayout('NamespacedUDVT_V2_Ok', true);
+  const comparison = getStorageUpgradeErrors(v1, v2);
+  t.deepEqual(comparison, []);
 });
 
 test('user defined value types - no layout info', async t => {
-  const v1 = t.context.extractStorageLayout('NamespacedUDVT');
-  const v2 = t.context.extractStorageLayout('NamespacedUDVT_V2_Ok');
+  const v1 = t.context.extractStorageLayout('NamespacedUDVT', false);
+  const v2 = t.context.extractStorageLayout('NamespacedUDVT_V2_Ok', false);
   const comparison = getStorageUpgradeErrors(v1, v2);
   t.deepEqual(comparison, []);
 });
 
 test('user defined value types - layout info - bad underlying type', async t => {
-  const v1 = t.context.extractStorageLayout('NamespacedUDVT');
-  const v2 = t.context.extractStorageLayout('NamespacedUDVT_V2_Resize');
+  const v1 = t.context.extractStorageLayout('NamespacedUDVT', true);
+  const v2 = t.context.extractStorageLayout('NamespacedUDVT_V2_Resize', true);
   const comparison = getStorageUpgradeErrors(v1, v2);
   t.like(comparison, {
     length: 1,
@@ -100,16 +110,33 @@ test('user defined value types - layout info - bad underlying type', async t => 
   });
 });
 
+test('user defined value types - no layout info - bad underlying type', async t => {
+  const v1 = t.context.extractStorageLayout('NamespacedUDVT', false);
+  const v2 = t.context.extractStorageLayout('NamespacedUDVT_V2_Resize', false);
+  const comparison = getStorageUpgradeErrors(v1, v2);
+  t.like(comparison, {
+    length: 1,
+    0: {
+      kind: 'typechange',
+      change: {
+        kind: 'unknown',
+      },
+      original: { label: 'my_user_value' },
+      updated: { label: 'my_user_value' },
+    },
+  });
+});
+
 test('mapping with user defined value type key - ok', t => {
-  const v1 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V1');
-  const v2 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V2_Ok');
+  const v1 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V1', true);
+  const v2 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V2_Ok', true);
   const comparison = getStorageUpgradeErrors(v1, v2);
   t.deepEqual(comparison, []);
 });
 
 test('mapping with user defined value type key - bad', t => {
-  const v1 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V1');
-  const v2 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V2_Bad');
+  const v1 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V1', true);
+  const v2 = t.context.extractStorageLayout('NamespacedUDVT_MappingKey_V2_Bad', true);
   const comparison = getStorageUpgradeErrors(v1, v2);
   t.like(comparison, {
     length: 2,
