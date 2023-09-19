473b6428a92b4a6c68bc73dcefa94e618fac3685
Eric Lau <ericglau@outlook.com>
2023-09-19 15:22:51 -0400

    Add unit test for makeNamespacedInputCopy

---
+++ packages/core/src/storage/namespace.test.ts
@@ -0,0 +1,14 @@
+import test from 'ava';
+import { artifacts } from 'hardhat';
+
+import { makeNamespacedInputCopy } from './namespace';
+
+test('make namespaced input copy', async t => {
+  const origBuildInfo = await artifacts.getBuildInfo('contracts/test/Namespaced.sol:Example');
+  if (origBuildInfo === undefined) {
+    throw new Error('Build info not found');
+  }
+
+  const modifiedInput = makeNamespacedInputCopy(origBuildInfo.input, origBuildInfo.output);
+  t.snapshot(modifiedInput);
+});
