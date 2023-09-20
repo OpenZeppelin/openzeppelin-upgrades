48e630ffde9ff3cdab3cfdaecef4f0c299a00a4d
Eric Lau <ericglau@outlook.com>
2023-09-20 13:46:22 -0400

    Remove parent constructor invocations - WIP

--- packages/core/src/utils/make-namespaced.ts
+++ packages/core/src/utils/make-namespaced.ts
@@ -15,7 +15,7 @@ const OUTPUT_SELECTION = {
  * so that the compiler will generate their types in the storage layout.
  *
  * This deletes all functions for efficiency, since they are not needed for storage layout.
- * We also need to delete modifiers and immutable variables to avoid compilation errors due to deleted
+ * We also need to delete modifiers, variables, and parent constructor invocations to avoid compilation errors due to deleted
  * functions and constructors.
  *
  * Also sets the outputSelection to only include storageLayout and ast, since the other outputs are not needed.
@@ -35,9 +35,19 @@ export function makeNamespacedInput(input: SolcInput, output: SolcOutput): SolcI
       continue;
     }
 
+    const orig = Buffer.from(source.content, 'utf8');
+
     const modifications: Modification[] = [];
 
     for (const contractDef of findAll('ContractDefinition', output.sources[sourcePath].ast)) {
+      // Remove any calls to parent constructors from the inheritance list
+      const inherits = contractDef.baseContracts;
+      for (const inherit of inherits) {
+        if (isNodeType('InheritanceSpecifier', inherit)) {
+          modifications.push(getReplace(inherit, inherit.baseName.name ?? '')); // TODO handle when name is undefined (for UserDefinedTypeName)
+        }
+      }
+
       const nodes = contractDef.nodes;
       for (const node of nodes) {
         if (
@@ -47,9 +57,9 @@ export function makeNamespacedInput(input: SolcInput, output: SolcOutput): SolcI
         ) {
           const doc = node.documentation;
           if (doc) {
-            modifications.push(getDelete(doc));
+            modifications.push(getDelete(doc, orig));
           }
-          modifications.push(getDelete(node));
+          modifications.push(getDelete(node, orig));
         } else if (isNodeType('StructDefinition', node)) {
           const storageLocationArg = getStorageLocationArg(node);
           if (storageLocationArg !== undefined) {
@@ -63,7 +73,7 @@ export function makeNamespacedInput(input: SolcInput, output: SolcOutput): SolcI
       }
     }
 
-    modifiedSources[sourcePath] = { ...source, content: getModifiedSource(source.content, modifications) };
+    modifiedSources[sourcePath] = { ...source, content: getModifiedSource(orig, modifications) };
   }
 
   return { ...input, sources: modifiedSources, settings: { ...input.settings, outputSelection: OUTPUT_SELECTION } };
@@ -72,7 +82,6 @@ export function makeNamespacedInput(input: SolcInput, output: SolcOutput): SolcI
 interface Modification {
   start: number;
   end: number;
-  kind: 'insert' | 'delete';
   text?: string;
 }
 
@@ -82,40 +91,38 @@ function getPositions(node: Node) {
   return { start, end };
 }
 
+function getReplace(node: Node, text: string): Modification {
+  const { start, end } = getPositions(node);
+  return { start, end, text };
+}
+
 function getInsert(node: Node, text: string): Modification {
   const { end } = getPositions(node);
-  return { start: end, end: end, kind: 'insert' as const, text };
+  return { start: end, end, text };
 }
 
-function getDelete(node: Node): Modification {
-  const { start, end } = getPositions(node);
-  return { start, end, kind: 'delete' as const };
+function getDelete(node: Node, orig: Buffer): Modification {
+  const positions = getPositions(node);
+  let end = positions.end;
+  // If the next character is a semicolon (e.g. for variables), skip over it
+  if (end + 1 < orig.length && orig.subarray(end, end + 1).toString() === ';') {
+    end += 1;
+  }
+  return { start: positions.start, end };
 }
 
-function getModifiedSource(sourceContent: string, modifications: Modification[]): string {
-  const orig = Buffer.from(sourceContent, 'utf8');
-
+function getModifiedSource(orig: Buffer, modifications: Modification[]): string {
   let result = '';
   let copyFromIndex = 0;
 
   for (const modification of modifications) {
     result += orig.subarray(copyFromIndex, modification.start).toString();
 
-    if (modification.kind === 'insert') {
+    if (modification.text !== undefined) {
       result += modification.text;
-      copyFromIndex = modification.end;
-    } else {
-      // delete
-      if (
-        modification.end + 1 < orig.length &&
-        orig.subarray(modification.end, modification.end + 1).toString() === ';'
-      ) {
-        // If the next character is a semicolon (e.g. for variables), skip over it
-        copyFromIndex = modification.end + 1;
-      } else {
-        copyFromIndex = modification.end;
-      }
     }
+
+    copyFromIndex = modification.end;
   }
 
   if (copyFromIndex < orig.length) {
