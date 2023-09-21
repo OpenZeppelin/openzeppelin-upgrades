072b12b493b976827496bf59f8f9d216f63e3e22
Eric Lau <ericglau@outlook.com>
2023-09-21 10:00:36 -0400

    Update packages/core/src/utils/make-namespaced.ts
    Co-authored-by: Francisco <fg@frang.io>

--- packages/core/src/utils/make-namespaced.ts
+++ packages/core/src/utils/make-namespaced.ts
@@ -44,16 +44,8 @@ export function makeNamespacedInput(input: SolcInput, output: SolcOutput): SolcI
       const inherits = contractDef.baseContracts;
       for (const inherit of inherits) {
         if (isNodeType('InheritanceSpecifier', inherit)) {
-          const baseName = inherit.baseName;
-
-          let name;
-          if (isNodeType('UserDefinedTypeName', baseName)) {
-            name = (baseName.name ?? baseName.pathNode?.name)!;
-          } else {
-            name = baseName.name;
-          }
-
-          modifications.push(getReplace(inherit, name));
+          assert(inherit.baseName.name !== undefined);
+          modifications.push(getReplace(inherit, inherit.baseName.name));
         }
       }
 
