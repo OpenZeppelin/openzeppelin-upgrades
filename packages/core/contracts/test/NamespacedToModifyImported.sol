0a49f9b1331ed66ffcb099956727fc809d6540d5
Eric Lau <ericglau@outlook.com>
2023-11-08 12:03:01 -0500

    Add overloaded import

--- packages/core/contracts/test/NamespacedToModifyImported.sol
+++ packages/core/contracts/test/NamespacedToModifyImported.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {CONSTANT_USING_SELECTOR, plusTwo} from "./NamespacedToModify.sol";
+import {CONSTANT_USING_SELECTOR, plusTwo, plusThree} from "./NamespacedToModify.sol";
 
 contract Example {}
