7060df91ea40cedf2af45ed8002d939213c4309f
Eric Lau <ericglau@outlook.com>
2023-11-22 14:40:02 -0500

    Clarify broken contract in tests

--- packages/core/contracts/test/Proxiable.sol
+++ packages/core/contracts/test/Proxiable.sol
@@ -3,28 +3,22 @@ pragma solidity ^0.8.2;
 
 // These contracts are for testing only, they are not safe for use in production.
 
-abstract contract UUPSUpgradeable {
+abstract contract BrokenProxiable {
     string public constant UPGRADE_INTERFACE_VERSION = "5.0.0";
 
+    // NOT SAFE FOR PRODUCTION USE.
+    // This does NOT actually perform any upgrade, but is only for tests to check that this function exists.
     function upgradeToAndCall(address newImplementation, bytes calldata data) external {
         _authorizeUpgrade(newImplementation);
-        if (data.length > 0) {
-            (bool success, ) = address(this).call(data);
-            require(success, "upgrade call reverted");
-        }
     }
 
-    function _authorizeUpgrade(address newImplementation) internal virtual;
-}
-
-abstract contract Proxiable is UUPSUpgradeable {
-    function _authorizeUpgrade(address newImplementation) internal override {
+    function _authorizeUpgrade(address newImplementation) internal {
         _beforeUpgrade(newImplementation);
     }
 
     function _beforeUpgrade(address newImplementation) internal virtual;
 }
 
-contract ChildOfProxiable is Proxiable {
+contract ChildOfProxiable is BrokenProxiable {
     function _beforeUpgrade(address newImplementation) internal virtual override {}
 }
