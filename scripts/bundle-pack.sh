d849b907cc694cb811d1d55c2d97ee4522ead6d2
Eric Lau <ericglau@outlook.com>
2024-01-09 18:34:28 -0500

    Fix action

--- scripts/bundle-pack.sh
+++ scripts/bundle-pack.sh
@@ -8,17 +8,17 @@ yarn prepare
 
 mv packages/core/node_modules core-node_modules
 
-for t in hardhat truffle; do
-  mkdir -p "packages/plugin-$t/node_modules/@openzeppelin"
-  if [ ! -e "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core" ]; then
-    ln -sr packages/core "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core"
-  fi
-  cd "packages/plugin-$t"
-  sed -i.bak -e 's/^}$/,"bundledDependencies":["@openzeppelin\/upgrades-core"]}/' package.json
-  npm pack --ignore-scripts
-  mv package.json.bak package.json
-  cd ../..
-done
+t=hardhat
+
+mkdir -p "packages/plugin-$t/node_modules/@openzeppelin"
+if [ ! -e "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core" ]; then
+  ln -sr packages/core "packages/plugin-$t/node_modules/@openzeppelin/upgrades-core"
+fi
+cd "packages/plugin-$t"
+sed -i.bak -e 's/^}$/,"bundledDependencies":["@openzeppelin\/upgrades-core"]}/' package.json
+npm pack --ignore-scripts
+mv package.json.bak package.json
+cd ../..
 
 mv core-node_modules packages/core/node_modules
 
