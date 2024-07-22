67fe9f2f4dae57f8609fcbdb476da6857ce05f95
Eric Lau <ericglau@outlook.com>
2024-07-22 16:33:32 -0400

    Remove unneeded code

--- packages/core/src/utils/slang/vendored/operatingSystem.ts
+++ packages/core/src/utils/slang/vendored/operatingSystem.ts
@@ -1,22 +1,5 @@
 // from https://github.com/NomicFoundation/hardhat-vscode/blob/development/server/src/utils/operatingSystem.ts
-import { exec } from 'child_process';
 import os from 'os';
-
-export function runningOnWindows() {
-  return os.platform() === 'win32';
-}
-
-export async function runCmd(cmd: string, cwd?: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    exec(cmd, { cwd }, function (error, stdout) {
-      if (error !== null) {
-        reject(error);
-      }
-
-      resolve(stdout);
-    });
-  });
-}
 
 export function getPlatform() {
   return `${os.platform()}-${os.arch()}`;
