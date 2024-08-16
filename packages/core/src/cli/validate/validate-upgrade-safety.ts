c5013c8a9ace57cc3be2cc08631c88036da91d85
Eric Lau <ericglau@outlook.com>
2024-08-16 09:42:03 -0400

    Refactor

--- packages/core/src/cli/validate/validate-upgrade-safety.ts
+++ packages/core/src/cli/validate/validate-upgrade-safety.ts
@@ -23,20 +23,20 @@ export type SpecifiedContracts = {
  * Only contracts that are detected as upgradeable will be validated.
  *
  * @param buildInfoDir Path of build info directory, or undefined to use the default Hardhat or Foundry build-info dir.
- * @param referenceBuildInfoDirs Paths of reference build info directories, which can be referred to in the `reference` option
- *   or in the `@custom:oz-upgrades-from` annotation using prefix `<directoryName>:` before the contract name or fully qualified name.
- *   Each directory (including the main build info directory) must have a unique name.
  * @param contract The name or fully qualified name of the contract to validate. If not specified, all upgradeable contracts in the build info directory will be validated.
  * @param reference The name or fully qualified name of the reference contract to use for storage layout comparisons. Can only be used along with `contract`. If not specified, uses the `@custom:oz-upgrades-from` annotation in the contract that is being validated.
  * @param opts Validation options, or undefined to use the default validation options.
- * @returns The project report.
+ * @param referenceBuildInfoDirs Paths of reference build info directories, which can be referred to in the `reference` option
+ *   or in the `@custom:oz-upgrades-from` annotation using prefix `<directoryName>:` before the contract name or fully qualified name.
+ *   Each directory (including the main build info directory) must have a unique name.
+* @returns The project report.
  */
 export async function validateUpgradeSafety(
   buildInfoDir?: string,
-  referenceBuildInfoDirs?: string[],
   contract?: string,
   reference?: string,
   opts: ValidateUpgradeSafetyOptions = {},
+  referenceBuildInfoDirs?: string[],
 ): Promise<ProjectReport> {
   const allOpts = withCliDefaults(opts);
 
@@ -44,13 +44,15 @@ export async function validateUpgradeSafety(
   const sourceContracts = validateBuildInfoContracts(buildInfoFiles);
 
   const dictionary: ReferenceBuildInfoDictionary = {};
-  for (const referenceBuildInfoDir of referenceBuildInfoDirs ?? []) {
-    if (dictionary[referenceBuildInfoDir] !== undefined) {
-      throw new Error(`Build info directory names must be unique. Found duplicate name: ${referenceBuildInfoDir}`);
-    }
+  if (referenceBuildInfoDirs !== undefined) {
+    for (const referenceBuildInfoDir of referenceBuildInfoDirs) {
+      if (dictionary[referenceBuildInfoDir] !== undefined) {
+        throw new Error(`Build info directory names must be unique. Found duplicate name: ${referenceBuildInfoDir}`);
+      }
 
-    const referenceBuildInfoFiles = await getBuildInfoFiles(referenceBuildInfoDir);
-    dictionary[referenceBuildInfoDir] = validateBuildInfoContracts(referenceBuildInfoFiles);
+      const referenceBuildInfoFiles = await getBuildInfoFiles(referenceBuildInfoDir);
+      dictionary[referenceBuildInfoDir] = validateBuildInfoContracts(referenceBuildInfoFiles);
+    }
   }
 
   const specifiedContracts = findSpecifiedContracts(sourceContracts, allOpts, contract, reference, dictionary);
