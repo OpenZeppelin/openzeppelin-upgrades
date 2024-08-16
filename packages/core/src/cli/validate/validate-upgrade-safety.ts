cdd31a0391b46f0623ab6aedfba4f2888f7c7024
Eric Lau <ericglau@outlook.com>
2024-08-16 11:07:40 -0400

    Update docs

--- packages/core/src/cli/validate/validate-upgrade-safety.ts
+++ packages/core/src/cli/validate/validate-upgrade-safety.ts
@@ -27,7 +27,7 @@ export type SpecifiedContracts = {
  * @param contract The name or fully qualified name of the contract to validate. If not specified, all upgradeable contracts in the build info directory will be validated.
  * @param reference The name or fully qualified name of the reference contract to use for storage layout comparisons. Can only be used along with `contract`. If not specified, uses the `@custom:oz-upgrades-from` annotation in the contract that is being validated.
  * @param opts Validation options, or undefined to use the default validation options.
- * @param referenceBuildInfoDirs Optional paths of additional build info directories from previous versions of the project to use for storage layout comparison. When using this option, refer to one of these directories using prefix `<dirName>:` before the contract name or fully qualified name in the `reference` param or `@custom:oz-upgrades-from` annotation, where `<dirName>` is the directory short name. Each directory short name must be unique.
+ * @param referenceBuildInfoDirs Optional paths of additional build info directories from previous versions of the project to use for storage layout comparisons. When using this option, refer to one of these directories using prefix `<dirName>:` before the contract name or fully qualified name in the `reference` param or `@custom:oz-upgrades-from` annotation, where `<dirName>` is the directory short name. Each directory short name must be unique.
  * @returns The project report.
  */
 export async function validateUpgradeSafety(
