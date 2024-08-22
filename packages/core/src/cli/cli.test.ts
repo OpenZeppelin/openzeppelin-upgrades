dc62a3c93c1b4ce89468b9bd77cafc02de6e0d69
Eric Lau <ericglau@outlook.com>
2024-08-22 13:47:58 -0400

    Update docs, use double quotes

--- packages/core/src/cli/cli.test.ts
+++ packages/core/src/cli/cli.test.ts
@@ -578,7 +578,7 @@ test('validate - excludes by pattern - no match', async t => {
   const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/excludes/UsesAbstractUUPS.sol:UsesAbstractUUPS`);
   await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
 
-  const error = await t.throwsAsync(execAsync(`${CLI} validate ${temp} --exclude '**/NoMatch.sol'`));
+  const error = await t.throwsAsync(execAsync(`${CLI} validate ${temp} --exclude "**/NoMatch.sol"`));
   const expectation: string[] = [`Stdout: ${(error as any).stdout}`, `Stderr: ${(error as any).stderr}`];
   t.snapshot(expectation.join('\n'));
 });
@@ -588,7 +588,7 @@ test('validate - excludes by pattern - some match', async t => {
   const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/excludes/UsesAbstractUUPS.sol:UsesAbstractUUPS`);
   await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
 
-  const error = await t.throwsAsync(execAsync(`${CLI} validate ${temp} --exclude '**/Abstract*.sol'`));
+  const error = await t.throwsAsync(execAsync(`${CLI} validate ${temp} --exclude "**/Abstract*.sol"`));
   const expectation: string[] = [`Stdout: ${(error as any).stdout}`, `Stderr: ${(error as any).stderr}`];
   t.snapshot(expectation.join('\n'));
 });
@@ -598,7 +598,7 @@ test('validate - excludes by pattern - all match', async t => {
   const buildInfo = await artifacts.getBuildInfo(`contracts/test/cli/excludes/UsesAbstractUUPS.sol:UsesAbstractUUPS`);
   await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
 
-  const output = await execAsync(`${CLI} validate ${temp} --exclude '**/excludes/*.sol'`);
+  const output = await execAsync(`${CLI} validate ${temp} --exclude "**/excludes/*.sol"`);
   t.snapshot(output);
 });
 
@@ -608,7 +608,7 @@ test('validate - excludes specified contract', async t => {
   await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
 
   const error = await t.throwsAsync(execAsync(
-    `${CLI} validate ${temp} --contract BecomesBadLayout --reference StorageV1 --exclude '**/Validate.sol'`,
+    `${CLI} validate ${temp} --contract BecomesBadLayout --reference StorageV1 --exclude "**/Validate.sol"`,
   ));
   t.true(error?.message.includes('No validation report found for contract contracts/test/cli/Validate.sol:BecomesBadLayout'), error?.message);
 });
@@ -619,7 +619,7 @@ test('validate - excludes one contract from layout comparisions', async t => {
   await fs.writeFile(path.join(temp, 'validate.json'), JSON.stringify(buildInfo));
 
   const error = await t.throwsAsync(execAsync(
-    `${CLI} validate ${temp} --exclude '**/V2Bad1.sol'`,
+    `${CLI} validate ${temp} --exclude "**/V2Bad1.sol"`,
   ));
   const expectation: string[] = [`Stdout: ${(error as any).stdout}`, `Stderr: ${(error as any).stderr}`];
   t.snapshot(expectation.join('\n'));
