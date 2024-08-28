40c76c0c31c049efd4c1923feba6ac6a146ab646
Eric Lau <ericglau@outlook.com>
2024-08-28 16:00:20 -0400

    Use string[] constant for defaultExclude

--- packages/core/src/cli/validate/default-exclude.ts
+++ packages/core/src/cli/validate/default-exclude.ts
@@ -1,3 +1,3 @@
 const UPGRADEABLE_BEACON = '**/contracts/proxy/beacon/UpgradeableBeacon.sol' as const;
 
-export const defaultExclude = [UPGRADEABLE_BEACON];
+export const defaultExclude: string[] = [UPGRADEABLE_BEACON] as const;
