bf63f0f4f34de457faaae1db62bda9da8e5e7836
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2024-12-02 11:26:25 -0300

    Adds test for deployBeaconProxy customization + Fix doc
    - Added a test case for the customization of the BeaconProxy.
    - Fixed the documentation, moving the new options to common options
      since they are used both in deployProxy and deployBeaconProxy.

---
+++ packages/plugin-hardhat/contracts/CustomBeaconProxy.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+
+// Example of a custom beacon proxy, for testing only.
+
+contract CustomBeaconProxy is BeaconProxy {
+    address private immutable _deployer;
+    // The beacon that will be used on calls by the deployer address
+    address private immutable _deployerBeacon;
+
+    constructor(address beacon, bytes memory data, address deployerBeacon) payable BeaconProxy(beacon, data) {
+        _deployer = msg.sender;
+        _deployerBeacon = deployerBeacon;
+    }
+
+    function _getBeacon() internal view override returns (address) {
+        if (msg.sender == _deployer) return _deployerBeacon;
+        return super._getBeacon();
+    }
+}
