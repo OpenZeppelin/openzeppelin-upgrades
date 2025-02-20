e3e65ce94c36d7f4cf4321cd6d885b82ceb2d104
CoveMB <bmarquiscom@gmail.com>
2025-02-20 12:31:55 -0500

    add cross repository trigger

--- scripts/prepare-foundry-docs.sh
+++ scripts/prepare-foundry-docs.sh
@@ -2,5 +2,3 @@
 
 rm -rf docs/modules/ROOT/pages/foundry
 cp -r submodules/openzeppelin-foundry-upgrades/docs/modules docs/modules/ROOT/pages/foundry
-# Checks if Foundry docs are up to date. If this fails, commit the changes from the above commands.
-git diff --exit-code docs/modules/ROOT/pages/foundry
