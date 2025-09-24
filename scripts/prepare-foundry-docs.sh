263a2d4a2cb6fad0f78d34ee01207e7f4a249ff7
Nikita Khateev <nikita.khateev@openzeppelin.com>
2025-09-24 18:24:01 +0400

    add recursive module checkout

--- scripts/prepare-foundry-docs.sh
+++ scripts/prepare-foundry-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
+git submodule update --init --recursive
 rm -rf docs/modules/ROOT/pages/foundry
 cp -r submodules/openzeppelin-foundry-upgrades/docs/modules docs/modules/ROOT/pages/foundry
