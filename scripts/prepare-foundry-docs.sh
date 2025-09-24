acd825519522b546d5c095de78f49dcb196982dc
Nikita Khateev <nikita.khateev@openzeppelin.com>
2025-09-24 19:20:09 +0400

    get submodule back

--- scripts/prepare-foundry-docs.sh
+++ scripts/prepare-foundry-docs.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-git submodule add https://github.com/OpenZeppelin/openzeppelin-foundry-upgrades submodules/openzeppelin-foundry-upgrades
 rm -rf docs/modules/ROOT/pages/foundry
 cp -r submodules/openzeppelin-foundry-upgrades/docs/modules docs/modules/ROOT/pages/foundry
