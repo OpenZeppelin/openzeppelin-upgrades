1a16b9d31b4257f92bba084c609eefddde2e1bc9
Arthur Abeilice <afa7789@gmail.com>
2025-10-22 11:49:35 -0300

    chore(plugin): refactor hooks to improve user config resolution and enhance artifact cleanup process

--- packages/plugin-hardhat/src/hooks/config.ts
+++ packages/plugin-hardhat/src/hooks/config.ts
@@ -2,30 +2,39 @@ import type { ConfigHooks } from 'hardhat/types/hooks';
 
 export default async (): Promise<Partial<ConfigHooks>> => {
   return {
-    afterResolveConfig(context) {
-      const { config } = context;
+    async resolveUserConfig(userConfig, resolveConfigurationVariable, next) {
+      // First, let other plugins resolve the config
+      const config = await next(userConfig, resolveConfigurationVariable);
       
-      // Accumulate references to all the compiler settings, including overrides
-      const settings = [];
-      for (const compiler of config.solidity.compilers) {
-        compiler.settings ??= {};
-        settings.push(compiler.settings);
-      }
-      for (const compilerOverride of Object.values(config.solidity.overrides)) {
-        compilerOverride.settings ??= {};
-        settings.push(compilerOverride.settings);
-      }
+      // Iterate through all profiles
+      for (const profile of Object.values(config.solidity.profiles)) {
+        // Accumulate references to all the compiler settings, including overrides
+        const settings = [];
+        
+        for (const compiler of profile.compilers) {
+          compiler.settings ??= {};
+          settings.push(compiler.settings);
+        }
+        
+        for (const compilerOverride of Object.values(profile.overrides)) {
+          compilerOverride.settings ??= {};
+          settings.push(compilerOverride.settings);
+        }
 
-      // Enable storage layout in all of them
-      for (const setting of settings) {
-        setting.outputSelection ??= {};
-        setting.outputSelection['*'] ??= {};
-        setting.outputSelection['*']['*'] ??= [];
+        // Enable storage layout in all of them
+        // TODO: Review this, not sure about the storage layout yet.
+        for (const setting of settings) {
+          setting.outputSelection ??= {};
+          setting.outputSelection['*'] ??= {};
+          setting.outputSelection['*']['*'] ??= [];
 
-        if (!setting.outputSelection['*']['*'].includes('storageLayout')) {
-          setting.outputSelection['*']['*'].push('storageLayout');
+          if (!setting.outputSelection['*']['*'].includes('storageLayout')) {
+            setting.outputSelection['*']['*'].push('storageLayout');
+          }
         }
       }
+
+      return config;
     },
   };
 };
