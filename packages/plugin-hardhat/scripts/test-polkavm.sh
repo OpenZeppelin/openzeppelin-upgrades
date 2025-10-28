9aad8a298fb4cc9b23da9553b97bc28d032317e9
Nikita Khateev <nikita.khateev@openzeppelin.com>
2025-10-28 14:35:00 +0400

    fix sed invocation

--- packages/plugin-hardhat/scripts/test-polkavm.sh
+++ packages/plugin-hardhat/scripts/test-polkavm.sh
@@ -2,9 +2,6 @@
 set -euo pipefail
 
 rimraf .openzeppelin
-# Replace all pragma lines in .sol files with pragma solidity ^0.8.20;
-find contracts -name "*.sol" -exec sed -i '' 's/^pragma solidity ^0\.[^8].*/pragma solidity ^0.8.20;/' {} \;
-# Check if the system is macOS or Linux based and architecture
 os=$(uname -s)
 if [[ "$os" == "Linux" ]]; then
     system="linux"
@@ -18,6 +15,16 @@ if [[ "$arch" == "x86_64" ]]; then
 elif [[ "$arch" == "aarch64" ]] || [[ "$arch" == "arm64" ]]; then
     arch="arm64"
 fi
+
+# Replace all pragma lines in .sol files with pragma solidity ^0.8.20;
+if [[ "$system" == "darwin" ]]; then
+    sed_inplace="-i ''"
+else
+    sed_inplace="-i"
+fi
+find contracts -name "*.sol" -exec sed $sed_inplace 's/^pragma solidity ^0\.[^8].*/pragma solidity ^0.8.20;/' {} \;
+# Check if the system is macOS or Linux based and architecture
+
 mkdir bin
 # Download eth-rpc binary
 curl -L https://github.com/paritytech/hardhat-polkadot/releases/download/nodes-17973191726/eth-rpc-${system}-${arch} -o bin/eth-rpc
