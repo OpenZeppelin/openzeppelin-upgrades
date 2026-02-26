b5870bb603ee9822d1b37b6573067d7521bc99da
Eric Lau <ericglau@outlook.com>
2026-02-26 16:33:58 -0500

    Update examples

--- packages/plugin-hardhat/example/BoxUUPS/hardhat.config.ts
+++ packages/plugin-hardhat/example/BoxUUPS/hardhat.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'hardhat/config';
-import hardhatEthers from '@nomicfoundation/hardhat-ethers';
 import hardhatUpgrades from '@openzeppelin/hardhat-upgrades';
 
 export default defineConfig({
-  plugins: [hardhatEthers, hardhatUpgrades],
+  plugins: [hardhatUpgrades],
   solidity: '0.8.28',
 });
